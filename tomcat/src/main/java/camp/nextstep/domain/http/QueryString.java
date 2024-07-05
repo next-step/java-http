6fa3337b806e43afb4a48e3cc6e547934d86f591
jinyoung <jinyoungchoi95@gmail.com>
2024-07-06 01:02:45 +0900

    refactor: QueryString 상수값 변경

--- tomcat/src/main/java/camp/nextstep/domain/http/QueryString.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/QueryString.java
@@ -6,20 +6,27 @@ import java.util.stream.Collectors;
 
 public class QueryString {
 
+    private static final String QUERY_STRING_FORMAT_SPLIT_REGEX = "&";
+    private static final String QUERY_PARAMETER_FORMAT_SPLIT_REGEX = "=";
+    private static final int QUERY_PARAMETER_FORMAT_LENGTH = 2;
+
+    private static final int QUERY_PARAMETER_KEY_INDEX = 0;
+    private static final int QUERY_PARAMETER_VALUE_INDEX = 1;
+
     private final Map<String, String> queryParameters;
 
     public QueryString(String queryString) {
-        this.queryParameters = Arrays.stream(queryString.split("&"))
+        this.queryParameters = Arrays.stream(queryString.split(QUERY_STRING_FORMAT_SPLIT_REGEX))
                 .map(QueryString::splitQueryParameter)
                 .collect(Collectors.toMap(
-                        queryParameter -> queryParameter[0],
-                        queryParameter -> queryParameter[1]
+                        queryParameter -> queryParameter[QUERY_PARAMETER_KEY_INDEX],
+                        queryParameter -> queryParameter[QUERY_PARAMETER_VALUE_INDEX]
                 ));
     }
 
     private static String[] splitQueryParameter(String queryParameter) {
-        String[] splitQueryParameter = queryParameter.split("=");
-        if (splitQueryParameter.length != 2) {
+        String[] splitQueryParameter = queryParameter.split(QUERY_PARAMETER_FORMAT_SPLIT_REGEX);
+        if (splitQueryParameter.length != QUERY_PARAMETER_FORMAT_LENGTH) {
             throw new IllegalArgumentException("QueryParameter값이 정상적으로 입력되지 않았습니다 - " + queryParameter);
         }
         return splitQueryParameter;
--- tomcat/src/test/java/camp/nextstep/domain/http/QueryStringTest.java
+++ tomcat/src/test/java/camp/nextstep/domain/http/QueryStringTest.java
@@ -13,7 +13,7 @@ class QueryStringTest {
     void key_value가_잘못된_형태로_입력된_경우_예외가_발생한다() {
         assertThatThrownBy(() -> new QueryString("name=jin=young"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("QueryString값이 정상적으로 입력되지 않았습니다 - name=jin=young");
+                .hasMessage("QueryParameter값이 정상적으로 입력되지 않았습니다 - name=jin=young");
     }
 
     @Test
