91807eb304228c9ba56316154b6dcf6b1ef8fd0c
jinyoung <jinyoungchoi95@gmail.com>
2024-07-05 23:33:53 +0900

    feat: HttpProtocol 각 요소 파싱 추가 및 테스트

--- tomcat/src/main/java/camp/nextstep/domain/http/HttpProtocol.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/HttpProtocol.java
@@ -2,10 +2,23 @@ package camp.nextstep.domain.http;
 
 public class HttpProtocol {
 
+    private final String protocol;
+    private final String version;
+
     public HttpProtocol(String httpProtocol) {
         String[] splitHttpProtocol = httpProtocol.split("/");
         if (splitHttpProtocol.length != 2) {
             throw new IllegalArgumentException("HttpProtocol값이 정상적으로 입력되지 않았습니다 - "+ httpProtocol);
         }
+        this.protocol = splitHttpProtocol[0];
+        this.version = splitHttpProtocol[1];
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getVersion() {
+        return version;
     }
 }
--- tomcat/src/test/java/camp/nextstep/domain/http/HttpProtocolTest.java
+++ tomcat/src/test/java/camp/nextstep/domain/http/HttpProtocolTest.java
@@ -2,7 +2,9 @@ package camp.nextstep.domain.http;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class HttpProtocolTest {
 
@@ -11,5 +13,14 @@ class HttpProtocolTest {
         assertThatThrownBy(() -> new HttpProtocol("HTTP"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("HttpProtocol값이 정상적으로 입력되지 않았습니다 - HTTP");
+    }
+
+    @Test
+    void protocol과_version을_파싱한다() {
+        HttpProtocol actual = new HttpProtocol("HTTP/1.1");
+        assertAll(
+                () -> assertThat(actual.getProtocol()).isEqualTo("HTTP"),
+                () -> assertThat(actual.getVersion()).isEqualTo("1.1")
+        );
     }
 }
