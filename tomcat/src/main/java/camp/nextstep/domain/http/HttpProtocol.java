dfd7ef975fbfc5b2cb6af959d5255ac9c1d9653d
jinyoung <jinyoungchoi95@gmail.com>
2024-07-05 23:35:32 +0900

    refactor: HttpProtocol 상수 분리 및 메소드 분리

--- tomcat/src/main/java/camp/nextstep/domain/http/HttpProtocol.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/HttpProtocol.java
@@ -2,16 +2,24 @@ package camp.nextstep.domain.http;
 
 public class HttpProtocol {
 
+    private static final String HTTP_PROTOCOL_FORMAT_SPLIT_REGEX = "/";
+    private static final int HTTP_PROTOCOL_FORMAT_LENGTH = 2;
+
     private final String protocol;
     private final String version;
 
     public HttpProtocol(String httpProtocol) {
-        String[] splitHttpProtocol = httpProtocol.split("/");
-        if (splitHttpProtocol.length != 2) {
-            throw new IllegalArgumentException("HttpProtocol값이 정상적으로 입력되지 않았습니다 - "+ httpProtocol);
-        }
+        String[] splitHttpProtocol = splitHttpProtocol(httpProtocol);
         this.protocol = splitHttpProtocol[0];
         this.version = splitHttpProtocol[1];
+    }
+
+    private static String[] splitHttpProtocol(String httpProtocol) {
+        String[] splitHttpProtocol = httpProtocol.split(HTTP_PROTOCOL_FORMAT_SPLIT_REGEX);
+        if (splitHttpProtocol.length != HTTP_PROTOCOL_FORMAT_LENGTH) {
+            throw new IllegalArgumentException("HttpProtocol값이 정상적으로 입력되지 않았습니다 - "+ httpProtocol);
+        }
+        return splitHttpProtocol;
     }
 
     public String getProtocol() {
