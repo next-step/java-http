8793bb2a260d74fd4fbbee1a62239d18a6ad5fc1
문주현 <hoopster@tmapmobility.com>
2024-07-05 23:01:05 +0900

    refactor : GreetingApplication refactoring

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -4,10 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static cache.com.example.version.CacheBustingWebConfig.ALL_PATH;
+import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final static String ALL_RESOURCE_PATH = PREFIX_STATIC_RESOURCES + ALL_PATH;
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new CacheControlInterceptor())
+                .excludePathPatterns(ALL_RESOURCE_PATH);
     }
 }
--- study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
+++ study/src/main/java/cache/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,23 @@
 package cache.com.example.etag;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import static cache.com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    private static final String ETAG_URL_PATTERN = "/etag";
+    private static final String RESOURCE_URL_PATTERN = PREFIX_STATIC_RESOURCES + "/*";
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        final FilterRegistrationBean<ShallowEtagHeaderFilter> filter = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
+        filter.addUrlPatterns(ETAG_URL_PATTERN);
+        filter.addUrlPatterns(RESOURCE_URL_PATTERN);
+        return filter;
+    }
 }
