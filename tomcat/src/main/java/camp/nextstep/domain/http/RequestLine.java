4caba88f4b638b4723f5f2bdab6ff70a92b45bae
jinyoung <jinyoungchoi95@gmail.com>
2024-07-05 22:44:19 +0900

    feat: RequestLine HttpMethod 지정 추가 및 테스트

--- tomcat/src/main/java/camp/nextstep/domain/http/RequestLine.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/RequestLine.java
@@ -2,9 +2,16 @@ package camp.nextstep.domain.http;
 
 public class RequestLine {
 
+    private final String httpMethod;
+
     public RequestLine(String requestLine) {
         if (!requestLine.equals("GET")) {
             throw new IllegalArgumentException("HttpStatus값이 존재하지 않습니다.");
         }
+        httpMethod = requestLine;
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
     }
 }
--- tomcat/src/test/java/camp/nextstep/domain/http/RequestLineTest.java
+++ tomcat/src/test/java/camp/nextstep/domain/http/RequestLineTest.java
@@ -2,6 +2,7 @@ package camp.nextstep.domain.http;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RequestLineTest {
@@ -9,5 +10,11 @@ class RequestLineTest {
     @Test
     void RequestLine에_status_GET이_없으면_예외가_발생한다() {
         assertThrowsExactly(IllegalArgumentException.class, () -> new RequestLine("ERROR"));
+    }
+
+    @Test
+    void RequestLine에서_status_GET을_파싱한다() {
+        RequestLine actual = new RequestLine("GET");
+        assertThat(actual.getHttpMethod()).isEqualTo("GET");
     }
 }
