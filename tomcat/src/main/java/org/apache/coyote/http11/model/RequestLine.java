55ddbb479b1b5a06ace7fc90492f412443ba5435
문주현 <hoopster@tmapmobility.com>
2024-07-06 01:08:29 +0900

    feature : add RequestLine model

---
+++ tomcat/src/main/java/org/apache/coyote/http11/model/RequestLine.java
@@ -0,0 +1,33 @@
+package org.apache.coyote.http11.model;
+
+import org.apache.coyote.http11.model.constant.HttpMethod;
+
+public class RequestLine {
+    private final HttpMethod httpMethod;
+    private final String url;
+    private final String protocol;
+    private final String version;
+
+    public RequestLine(final HttpMethod httpMethod, final String url, final String protocol, final String version) {
+        this.httpMethod = httpMethod;
+        this.url = url;
+        this.protocol = protocol;
+        this.version = version;
+    }
+
+    public HttpMethod httpMethod() {
+        return httpMethod;
+    }
+
+    public String url() {
+        return url;
+    }
+
+    public String protocol() {
+        return protocol;
+    }
+
+    public String version() {
+        return version;
+    }
+}
