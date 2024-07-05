eb494ba66983cbeb69760ec3f0f445b38ae45710
jinyoung <jinyoungchoi95@gmail.com>
2024-07-06 01:27:03 +0900

    refactor: HttpPath 상수값 변환

--- tomcat/src/main/java/camp/nextstep/domain/http/HttpPath.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/HttpPath.java
@@ -4,20 +4,27 @@ import java.util.Map;
 
 public class HttpPath {
 
+    private static final String HTTP_PATH_FORMAT_SPLIT_REGEX = "\\?";
+    private static final int HTTP_PATH_EMPTY_QUERY_STRING_FORMAT_LENGTH = 1;
+    private static final int HTTP_PATH_EXIST_QUERY_STRING_FORMAT_LENGTH = 2;
+
+    private static final int PATH_INDEX = 0;
+    private static final int QUERY_STRING_INDEX = 1;
+
     private final String path;
     private final QueryString queryString;
 
     public HttpPath(String httpPath) {
-        String[] splitHttpPath = httpPath.split("\\?");
-        this.path = splitHttpPath[0];
+        String[] splitHttpPath = httpPath.split(HTTP_PATH_FORMAT_SPLIT_REGEX);
+        this.path = splitHttpPath[PATH_INDEX];
         this.queryString = parseQueryString(httpPath, splitHttpPath);
     }
 
     private QueryString parseQueryString(String httpPath, String[] splitHttpPath) {
-        if (splitHttpPath.length == 2) {
-            return new QueryString(splitHttpPath[1]);
+        if (splitHttpPath.length == HTTP_PATH_EXIST_QUERY_STRING_FORMAT_LENGTH) {
+            return new QueryString(splitHttpPath[QUERY_STRING_INDEX]);
         }
-        if (splitHttpPath.length == 1) {
+        if (splitHttpPath.length == HTTP_PATH_EMPTY_QUERY_STRING_FORMAT_LENGTH) {
             return QueryString.empty();
         }
         throw new IllegalArgumentException("HttpPath값이 정상적으로 입력되지 않았습니다 - " + httpPath);
