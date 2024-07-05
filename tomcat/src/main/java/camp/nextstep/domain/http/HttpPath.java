4e2178bc6f2b1c63ce7bc6dfc5b0a82496a18b70
jinyoung <jinyoungchoi95@gmail.com>
2024-07-06 00:28:51 +0900

    feature: HttpPath path 비정상 값 입력 시 예외 추가 및 테스트

--- tomcat/src/main/java/camp/nextstep/domain/http/HttpPath.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/HttpPath.java
@@ -5,7 +5,11 @@ public class HttpPath {
     private final String path;
 
     public HttpPath(String httpPath) {
-        this.path = httpPath;
+        String[] splitHttpPath = httpPath.split("\\?");
+        if (splitHttpPath.length > 2) {
+            throw new IllegalArgumentException("HttpPath값이 정상적으로 입력되지 않았습니다 - " + httpPath);
+        }
+        this.path = splitHttpPath[0];
     }
 
     public String getPath() {
--- tomcat/src/test/java/camp/nextstep/domain/http/HttpPathTest.java
+++ tomcat/src/test/java/camp/nextstep/domain/http/HttpPathTest.java
@@ -3,6 +3,7 @@ package camp.nextstep.domain.http;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class HttpPathTest {
 
@@ -10,5 +11,12 @@ class HttpPathTest {
     void query_string이_없는_경우_path만_파싱한다() {
         HttpPath actual = new HttpPath("/docs/index.html");
         assertThat(actual.getPath()).isEqualTo("/docs/index.html");
+    }
+
+    @Test
+    void http_path의_포맷이_잘못된_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new HttpPath("/docs/index.html?name=value?name=value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("HttpPath값이 정상적으로 입력되지 않았습니다 - /docs/index.html?name=value?name=value");
     }
 }
