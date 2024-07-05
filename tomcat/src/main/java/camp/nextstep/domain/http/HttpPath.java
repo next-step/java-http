46137f0504cfd63427abc6d0ee565ab630637ead
jinyoung <jinyoungchoi95@gmail.com>
2024-07-06 00:31:50 +0900

    feature: HttpPath query string 파싱 추가 및 테스트

--- tomcat/src/main/java/camp/nextstep/domain/http/HttpPath.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/HttpPath.java
@@ -1,18 +1,32 @@
 package camp.nextstep.domain.http;
 
+import java.util.Map;
+
 public class HttpPath {
 
     private final String path;
+    private final Map<String, String> queryString;
 
     public HttpPath(String httpPath) {
         String[] splitHttpPath = httpPath.split("\\?");
-        if (splitHttpPath.length > 2) {
+        this.path = splitHttpPath[0];
+        if (splitHttpPath.length == 2) {
+            String[] queryString = splitHttpPath[1].split("=");
+            this.queryString = Map.of(queryString[0], queryString[1]);
+        }
+        else if (splitHttpPath.length == 1) {
+            this.queryString = Map.of();
+        }
+        else {
             throw new IllegalArgumentException("HttpPath값이 정상적으로 입력되지 않았습니다 - " + httpPath);
         }
-        this.path = splitHttpPath[0];
     }
 
     public String getPath() {
         return path;
+    }
+
+    public Map<String, String> getQueryString() {
+        return queryString;
     }
 }
--- tomcat/src/test/java/camp/nextstep/domain/http/HttpPathTest.java
+++ tomcat/src/test/java/camp/nextstep/domain/http/HttpPathTest.java
@@ -2,6 +2,8 @@ package camp.nextstep.domain.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -18,5 +20,11 @@ class HttpPathTest {
         assertThatThrownBy(() -> new HttpPath("/docs/index.html?name=value?name=value"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("HttpPath값이 정상적으로 입력되지 않았습니다 - /docs/index.html?name=value?name=value");
+    }
+
+    @Test
+    void query_string_구분자가_있는_경우_쿼리스트링을_파싱한다() {
+        HttpPath actual = new HttpPath("/docs/index.html?name=jinyoung");
+        assertThat(actual.getQueryString()).isEqualTo(Map.of("name", "jinyoung"));
     }
 }
