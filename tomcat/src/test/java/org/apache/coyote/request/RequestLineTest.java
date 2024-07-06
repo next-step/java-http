9f53ff3b9ffb10d134fba25f59a28a718d70d03d
Hyunta <mohai2618@gmail.com>
2024-07-06 21:54:01 +0900

    feature: POST 요청에 대해 파싱할 수 있다.

--- tomcat/src/test/java/org/apache/coyote/request/RequestLineTest.java
+++ tomcat/src/test/java/org/apache/coyote/request/RequestLineTest.java
@@ -12,14 +12,31 @@ class RequestLineTest {
     @DisplayName("HTTP GET 요청에 대한 RequestLine을 파싱할 수 있다.")
     void parseHttpGetRequest() {
         // given
-        final String request = "GET /users HTTP/1.1";
+        String request = "GET /users HTTP/1.1";
 
         // when
-        final RequestLine requestLine = RequestLine.parse(request);
+        RequestLine requestLine = RequestLine.parse(request);
 
         // then
         assertAll(
                 () -> assertThat(requestLine.getHttpMethod()).isEqualTo("GET"),
+                () -> assertThat(requestLine.getPath()).isEqualTo("/users"),
+                () -> assertThat(requestLine.getHttpProtocol()).isEqualTo("HTTP/1.1")
+        );
+    }
+
+    @Test
+    @DisplayName("HTTP POST 요청에 대한 RequestLine을 파싱할 수 있다.")
+    void parseHttpPostRequest() {
+        // given
+        String request = "POST /users HTTP/1.1";
+
+        // when
+        RequestLine requestLine = RequestLine.parse(request);
+
+        // then
+        assertAll(
+                () -> assertThat(requestLine.getHttpMethod()).isEqualTo("POST"),
                 () -> assertThat(requestLine.getPath()).isEqualTo("/users"),
                 () -> assertThat(requestLine.getHttpProtocol()).isEqualTo("HTTP/1.1")
         );
