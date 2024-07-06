a56216ce1074f676f577ebc30ada13927b28fadd
Sangmin <sm9171@nate.com>
2024-07-06 11:01:15 +0900

    test: 요구사항 3 - Query String 파싱

--- tomcat/src/main/java/camp/nextstep/io/RequestLine.java
+++ tomcat/src/main/java/camp/nextstep/io/RequestLine.java
@@ -1,15 +1,32 @@
 package camp.nextstep.io;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class RequestLine {
     private String method;
     private String path;
     private String protocol;
     private String version;
+    private List<Map> queryString = new ArrayList<>();
 
     public RequestLine(String request) {
         String[] tokens = request.split(" ");
         this.method = tokens[0];
-        this.path = tokens[1];
+
+        if (tokens[1].contains("?")) {
+            String[] pathAndQueryString = tokens[1].split("\\?");
+            this.path = pathAndQueryString[0];
+            String[] queryStrings = pathAndQueryString[1].split("&");
+            for (String queryString : queryStrings) {
+                String[] keyAndValue = queryString.split("=");
+                this.queryString.add(Map.of(keyAndValue[0], keyAndValue[1]));
+            }
+        } else {
+            this.path = tokens[1];
+        }
+
         String[] protocolAndVersion = tokens[2].split("/");
         this.protocol = protocolAndVersion[0];
         this.version = protocolAndVersion[1];
@@ -29,5 +46,9 @@ public class RequestLine {
 
     public String getVersion() {
         return version;
+    }
+
+    public List<Map> getQueryString() {
+        return queryString;
     }
 }
--- tomcat/src/test/java/camp/nextstep/io/RequestLineTest.java
+++ tomcat/src/test/java/camp/nextstep/io/RequestLineTest.java
@@ -38,4 +38,23 @@ class RequestLineTest {
                 () -> assertThat(requestLine.getVersion()).isEqualTo("1.1")
         );
     }
+
+    @DisplayName("요구사항 3 - Query String 파싱")
+    @Test
+    public void test3() {
+        //given
+        String request = "GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1";
+        //when
+        RequestLine requestLine = new RequestLine(request);
+        //then
+        assertAll(
+                () -> assertThat(requestLine.getMethod()).isEqualTo("GET"),
+                () -> assertThat(requestLine.getPath()).isEqualTo("/users"),
+                () -> assertThat(requestLine.getProtocol()).isEqualTo("HTTP"),
+                () -> assertThat(requestLine.getVersion()).isEqualTo("1.1"),
+                () -> assertThat(requestLine.getQueryString().get(0).get("userId")).isEqualTo("javajigi"),
+                () -> assertThat(requestLine.getQueryString().get(1).get("password")).isEqualTo("password"),
+                () -> assertThat(requestLine.getQueryString().get(2).get("name")).isEqualTo("JaeSung")
+        );
+    }
 }
