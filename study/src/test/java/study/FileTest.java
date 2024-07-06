040b5e54e28df08a4b792eacb7b8719a26b72ce1
junseo <djawnstj44@naver.com>
2024-07-07 03:00:23 +0900

    test: import 수정

--- study/src/test/java/study/FileTest.java
+++ study/src/test/java/study/FileTest.java
@@ -3,14 +3,13 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
