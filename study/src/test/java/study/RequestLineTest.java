cae6332958a166ae2b24906772a46a1dd3bded13
SeolYoungKim <nasur4da@gmail.com>
2024-07-07 00:37:05 +0900

    refactor(RequestLine): HTTP method에 enum 적용

--- study/src/test/java/study/RequestLineTest.java
+++ study/src/test/java/study/RequestLineTest.java
@@ -3,7 +3,12 @@ package study;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,13 +18,28 @@ import static org.assertj.core.api.Assertions.assertThat;
  * ex: GET /docs/index.html HTTP/1.1
  */
 public class RequestLineTest {
+    enum HttpMethod {
+        GET, POST;
+
+        private static final Map<String, HttpMethod> NAME_TO_HTTP_METHOD_MAP = Arrays.stream(values())
+                .collect(Collectors.toUnmodifiableMap(Enum::name, httpMethod -> httpMethod));
+
+        public static HttpMethod from(String method) {
+            String upperCase = method.toUpperCase();
+            if (NAME_TO_HTTP_METHOD_MAP.containsKey(upperCase)) {
+                return NAME_TO_HTTP_METHOD_MAP.get(upperCase);
+            }
+
+            throw new IllegalArgumentException("지원되지 않는 HTTP method. 입력=" + upperCase);
+        }
+    }
     static class RequestLine {
-        private final String method;
+        private final HttpMethod method;
         private final String path;
         private final String protocol;
         private final String version;
 
-        private RequestLine(final String method, final String path, final String protocol, final String version) {
+        private RequestLine(final HttpMethod method, final String path, final String protocol, final String version) {
             this.method = method;
             this.path = path;
             this.protocol = protocol;
@@ -31,11 +51,17 @@ public class RequestLineTest {
             String requestLine = requests[0];
             String[] requestLines = requestLine.split(" ");
 
+            String method = requestLines[0];
+            String path = requestLines[1];
+
             String[] protocolAndVersion = requestLines[2].split("/");
-            return new RequestLine(requestLines[0], requestLines[1], protocolAndVersion[0], protocolAndVersion[1]);
+            String protocol = protocolAndVersion[0];
+            String version = protocolAndVersion[1];
+
+            return new RequestLine(HttpMethod.from(method), path, protocol, version);
         }
 
-        public String getMethod() {
+        public HttpMethod getMethod() {
             return method;
         }
 
@@ -53,8 +79,8 @@ public class RequestLineTest {
     }
 
     @ParameterizedTest(name = "method = {0}")
-    @ValueSource(strings = {"GET", "POST"})
-    void 요청에_대한_RequestLine을_파싱한다(String method) {
+    @EnumSource(value = HttpMethod.class)
+    void 요청에_대한_RequestLine을_파싱한다(HttpMethod method) {
         final String request = """
                 %s /users HTTP/1.1
                 Host: www.nowhere123.com
