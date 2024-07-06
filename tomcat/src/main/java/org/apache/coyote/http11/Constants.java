00151a5a08f79230862221c5de1858fc419d5823
junseo <djawnstj44@naver.com>
2024-07-07 02:51:36 +0900

    feat: 상수 모음

---
+++ tomcat/src/main/java/org/apache/coyote/http11/Constants.java
@@ -0,0 +1,16 @@
+package org.apache.coyote.http11;
+
+public final class Constants {
+
+
+    /**
+     * CR.
+     */
+    public static final byte CR = (byte) '\r';
+
+
+    /**
+     * LF.
+     */
+    public static final byte LF = (byte) '\n';
+}
