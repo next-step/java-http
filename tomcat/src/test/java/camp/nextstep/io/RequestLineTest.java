a0e94c9ee0514d0fff38ae3732f794348737e1cf
Sangmin <sm9171@nate.com>
2024-07-06 10:51:04 +0900

    test: 요구사항 2 - POST 요청

--- tomcat/src/test/java/camp/nextstep/io/RequestLineTest.java
+++ tomcat/src/test/java/camp/nextstep/io/RequestLineTest.java
@@ -22,4 +22,20 @@ class RequestLineTest {
                 () -> assertThat(requestLine.getVersion()).isEqualTo("1.1")
         );
     }
+
+    @DisplayName("요구사항 2 - POST 요청")
+    @Test
+    public void test2() {
+        //given
+        String request = "POST /users HTTP/1.1";
+        //when
+        RequestLine requestLine = new RequestLine(request);
+        //then
+        assertAll(
+                () -> assertThat(requestLine.getMethod()).isEqualTo("POST"),
+                () -> assertThat(requestLine.getPath()).isEqualTo("/users"),
+                () -> assertThat(requestLine.getProtocol()).isEqualTo("HTTP"),
+                () -> assertThat(requestLine.getVersion()).isEqualTo("1.1")
+        );
+    }
 }
