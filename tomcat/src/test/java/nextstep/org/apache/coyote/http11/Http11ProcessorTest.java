311a12799b12ed423ab6d14ab9adc29f04cc8cb8
junseo <djawnstj44@naver.com>
2024-07-08 08:00:42 +0900

    test: index.html, styles.css 파일 반환 테스트 작성

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,11 +1,8 @@
 package nextstep.org.apache.coyote.http11;
 
-import camp.nextstep.servlet.LoginServlet;
-import org.apache.catalina.connector.CoyoteAdapter;
-import org.apache.coyote.http11.Http11Processor;
-import org.apache.coyote.http11.HttpInputParser;
-import org.junit.jupiter.api.Test;
 import support.StubSocket;
+import org.apache.coyote.http11.Http11Processor;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,19 +17,18 @@ class Http11ProcessorTest {
     void process() {
         // given
         final var socket = new StubSocket();
-        final var adapter = new CoyoteAdapter();
-        final var processor = new Http11Processor(socket, adapter);
+        final var processor = new Http11Processor(socket);
 
         // when
         processor.process(socket);
 
         // then
         var expected = String.join("\r\n",
-                "HTTP/1.1 500 Internal Server Error ",
+                "HTTP/1.1 200 OK ",
                 "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 11 ",
+                "Content-Length: 12 ",
                 "",
-                "ERROR: null");
+                "Hello world!");
 
         assertThat(socket.output()).isEqualTo(expected);
     }
@@ -48,8 +44,7 @@ class Http11ProcessorTest {
                 "");
 
         final var socket = new StubSocket(httpRequest);
-        final var adapter = new CoyoteAdapter();
-        final var processor = new Http11Processor(socket, adapter);
+        final Http11Processor processor = new Http11Processor(socket);
 
         // when
         processor.process(socket);
@@ -59,7 +54,7 @@ class Http11ProcessorTest {
         var expected = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 5518 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
+                "Content-Length: 5564 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
                 "",
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
 
@@ -77,8 +72,7 @@ class Http11ProcessorTest {
                 "");
 
         final var socket = new StubSocket(httpRequest);
-        final var adapter = new CoyoteAdapter();
-        final var processor = new Http11Processor(socket, adapter);
+        final Http11Processor processor = new Http11Processor(socket);
 
         // when
         processor.process(socket);
@@ -88,37 +82,7 @@ class Http11ProcessorTest {
         var expected = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: text/css;charset=utf-8 ",
-                "Content-Length: 211988 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
-                "",
-                new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
-
-        assertThat(socket.output()).isEqualTo(expected);
-    }
-
-    @Test
-    void login() throws IOException {
-        // given
-        final String httpRequest= String.join("\r\n",
-                "GET /login?account=gugu&password=password HTTP/1.1 ",
-                "Host: localhost:8080 ",
-                "Connection: keep-alive ",
-                "",
-                "");
-
-        final var socket = new StubSocket(httpRequest);
-        final var adapter = new CoyoteAdapter();
-        adapter.addServlet("/login", new LoginServlet());
-        final var processor = new Http11Processor(socket, adapter);
-
-        // when
-        processor.process(socket);
-
-        // then
-        final URL resource = getClass().getClassLoader().getResource("static/login.html");
-        var expected = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 3716 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
+                "Content-Length: 211991 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
                 "",
                 new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
 
