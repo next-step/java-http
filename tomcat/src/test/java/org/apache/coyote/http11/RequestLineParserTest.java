74be2011d3abc597d03bc563066b4547b8aaa782
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-07-07 11:13:28 +0900

    feat : 요구사항 1 - GET 요청

---
+++ tomcat/src/test/java/org/apache/coyote/http11/RequestLineParserTest.java
@@ -0,0 +1,30 @@
+package org.apache.coyote.http11;
+
+import org.apache.coyote.http11.RequestLine;
+import org.apache.coyote.http11.RequestLineParser;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
+
+@DisplayName("RequestLineParser class는")
+class RequestLineParserTest {
+
+    @DisplayName("GET 메서드를 통해 요청을 받았을 때 RequestLine 객체를 반환한다")
+    @Test
+    void parse_GET_RequestLine() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                                               "GET /users HTTP/1.1 ",
+                                               "");
+        final var socket = new StubSocket(httpRequest);
+        final var parser = new RequestLineParser(socket);
+
+        // when
+        final var result = parser.parse();
+        final var expected = RequestLine.from(httpRequest);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
