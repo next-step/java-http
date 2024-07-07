298c6273534633d6644100069cce677fe8f084f7
문주현 <hoopster@tmapmobility.com>
2024-07-08 01:38:39 +0900

    feature : logging login User info

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,8 +1,11 @@
 package org.apache.coyote.http11;
 
+import camp.nextstep.db.InMemoryUserRepository;
 import camp.nextstep.exception.UncheckedServletException;
+import camp.nextstep.model.User;
 import org.apache.coyote.Processor;
 import org.apache.coyote.http11.model.HttpRequestHeader;
+import org.apache.coyote.http11.model.QueryParams;
 import org.apache.coyote.http11.model.RequestLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,13 +18,17 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-    private static final HttpRequestHeaderParser HTTP_REQUEST_HEADER_PARSER = new HttpRequestHeaderParser();
     private static final String STATIC_PATH = "static";
     private static final String ROOT_PATH_RESPONSE_BODY = "Hello world!";
+    private static final String LOGIN_PATH = "/login.html";
+    private static final String LOGIN_ACCOUNT_KEY = "account";
+    private static final String LOGIN_PASSWORD_KEY = "password";
+    private static final HttpRequestHeaderParser HTTP_REQUEST_HEADER_PARSER = new HttpRequestHeaderParser();
 
     private final Socket connection;
 
@@ -46,9 +53,13 @@ public class Http11Processor implements Runnable, Processor {
                     .toList();
 
             final HttpRequestHeader httpRequestHeader = HTTP_REQUEST_HEADER_PARSER.parse(requestLines);
+            final RequestLine requestLine = httpRequestHeader.requestLine();
+
+            if (isLogin(requestLine)) {
+                loggingUser(requestLine.queryParams());
+            }
 
             final var responseBody = readHtmlResponseBody(httpRequestHeader.requestLine());
-
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
                     "Content-Type: " + httpRequestHeader.requestLine().contentTypeText() + ";charset=utf-8 ",
@@ -60,6 +71,20 @@ public class Http11Processor implements Runnable, Processor {
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        }
+    }
+
+    private boolean isLogin(final RequestLine requestLine) {
+        return requestLine.url()
+                .equals(LOGIN_PATH);
+    }
+
+    private void loggingUser(final QueryParams queryParams) {
+        final User user = InMemoryUserRepository.findByAccount(queryParams.valueBy(LOGIN_ACCOUNT_KEY))
+                .orElseThrow(NoSuchElementException::new);
+        
+        if (user.checkPassword(queryParams.valueBy(LOGIN_PASSWORD_KEY))) {
+            log.info("user {}", user);
         }
     }
 
