7a4799fdcc2c2196ade416e5ff999fd1f2ee8969
문주현 <hoopster@tmapmobility.com>
2024-07-07 13:59:55 +0900

    refactor : StringTokenizer using private

--- tomcat/src/main/java/org/apache/coyote/http11/StringTokenizer.java
+++ tomcat/src/main/java/org/apache/coyote/http11/StringTokenizer.java
@@ -1,6 +1,9 @@
 package org.apache.coyote.http11;
 
-public class StringTokenizer {
+public final class StringTokenizer {
+
+    private StringTokenizer() {
+    }
 
     public static String[] token(final String str, final String delimiter) {
         return str.split(delimiter);
--- tomcat/src/test/java/org/apache/coyote/http11/StringTokenizerTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/StringTokenizerTest.java
@@ -3,6 +3,7 @@ package org.apache.coyote.http11;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class StringTokenizerTest {
 
@@ -16,6 +17,11 @@ class StringTokenizerTest {
         final String[] result = StringTokenizer.token(testStr, delimiter);
 
         // then
-        assertThat(result.length).isEqualTo(3);
+        assertAll(
+                () -> assertThat(result.length).isEqualTo(3),
+                () -> assertThat(result[0]).isEqualTo("aaaa"),
+                () -> assertThat(result[1]).isEqualTo("ssd"),
+                () -> assertThat(result[2]).isEqualTo("dd")
+        );
     }
 }
