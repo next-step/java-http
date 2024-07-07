43256d64cc8d1aa55a7dc8a78ab1d06c25c0e427
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-07 21:48:08 +0900

    test(RequestLine): method `Post` 추출 테스트 추가

--- tomcat/src/test/java/camp/nextstep/http/domain/RequestLineTest.java
+++ tomcat/src/test/java/camp/nextstep/http/domain/RequestLineTest.java
@@ -9,10 +9,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class RequestLineTest {
 
     @Test
-    void requestLine_에서_Method_를_반환받을_수_있다() {
+    void requestLine_에서_GET_Method_를_반환받을_수_있다() {
         final RequestLine requestLine = new RequestLine("GET /users HTTP/1.1");
 
         assertThat(requestLine.getMethod()).isEqualTo("GET");
+    }
+
+    @Test
+    void requestLine_에서_POST_Method_를_반환받을_수_있다() {
+        final RequestLine requestLine = new RequestLine("POST /users HTTP/1.1");
+
+        assertThat(requestLine.getMethod()).isEqualTo("POST");
     }
 
     @Test
