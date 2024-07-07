91316b43f87db4f038a85a63a7ecef58e1431a35
jinyoung <jinyoungchoi95@gmail.com>
2024-07-07 19:37:53 +0900

    feat: root, html인 경우 해당 경로로 responseBody를 반환하도록 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
+import camp.nextstep.domain.http.RequestLine;
 import camp.nextstep.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -27,9 +33,11 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             final var inputReader = new BufferedReader(new InputStreamReader(inputStream));
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final var requestLine = new RequestLine(inputReader.readLine());
+            final var responseBody = parseResponseBody(requestLine.getHttpPath());
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -43,5 +51,19 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parseResponseBody(final String path) throws IOException {
+        if (path.equals("/")) {
+            return "Hello world!";
+        }
+        if (path.endsWith(".html")) {
+            return readFile(getClass().getClassLoader().getResource("static" + path));
+        }
+        throw new IllegalStateException("처리할 수 없습니다.");
+    }
+
+    private String readFile(URL resource) throws IOException {
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
     }
 }
