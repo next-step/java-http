ad1de0db6a861483075f7d24cd96ed0dfda9541e
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-07-07 11:13:56 +0900

    feat : 요구사항 2 - POST 요청

--- tomcat/src/test/java/org/apache/coyote/http11/RequestLineParserTest.java
+++ tomcat/src/test/java/org/apache/coyote/http11/RequestLineParserTest.java
@@ -27,4 +27,22 @@ class RequestLineParserTest {
         // then
         Assertions.assertThat(result).isEqualTo(expected);
     }
+
+    @DisplayName("POST 메서드를 통해 요청을 받았을 때 RequestLine 객체를 반환한다")
+    @Test
+    void parse_POST_RequestLine() {
+        // given
+        final String httpRequest = String.join("\r\n",
+                                               "POST /users HTTP/1.1 ",
+                                               "");
+        final var socket = new StubSocket(httpRequest);
+        final var parser = new RequestLineParser(socket);
+
+        // when
+        final var result = parser.parse();
+        final var expected = RequestLine.from(httpRequest);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
 }
