fe2da5711dd01dce80b6f0e2ff372353844d3db7
junseo <djawnstj44@naver.com>
2024-07-08 08:00:25 +0900

    feat: 정적 파일 반환 기능 추가

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,20 @@
 package org.apache.coyote.http11;
 
 import camp.nextstep.exception.UncheckedServletException;
+import jakarta.servlet.ServletContext;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,11 +37,34 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            final Http11Input http11Input = new Http11Input(inputStream);
+            http11Input.parseRequestLine();
+
+            final Request request = http11Input.getRequest();
+
+            var responseBody = "Hello world!";
+            // TODO 일급컬렉션 변경
+            final Map<String, String> responseHeaderMap = new HashMap<>();
+            responseHeaderMap.put("Content-Type", "text/html;charset=utf-8 ");
+
+            // TODO 역할 분리
+            final URL resource = getClass().getClassLoader().getResource("static/" + request.getPath());
+
+            if (resource != null) {
+                final Path path = new File(resource.getFile()).toPath();
+                responseBody = new String(Files.readAllBytes(path));
+                final String mimeType = Files.probeContentType(path);
+                responseHeaderMap.put("Content-Type", mimeType + ";charset=utf-8 ");
+            }
+
+            final String headers = responseHeaderMap.entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey() + ": " + entry.getValue())
+                    .collect(Collectors.joining("\r\n"));
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
+                    headers,
                     "Content-Length: " + responseBody.getBytes().length + " ",
                     "",
                     responseBody);
