2a09ff5b5e6dadd3d3284f10a454282ab100860d
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-07 22:25:14 +0900

    refactor(RequestLine): 매직리터럴 정리

--- tomcat/src/main/java/camp/nextstep/http/domain/RequestLine.java
+++ tomcat/src/main/java/camp/nextstep/http/domain/RequestLine.java
@@ -3,26 +3,29 @@ package camp.nextstep.http.domain;
 import camp.nextstep.http.exception.InvalidRequestLineException;
 
 public class RequestLine {
-
+    private static final int REQUIRED_REQUEST_LINE_LENGTH = 3;
     private static final int METHOD_INDEX = 0;
-    public static final int PATH_INDEX = 1;
+    private static final int PATH_INDEX = 1;
+    private static final int VERSION_INDEX = 2;
+    private static final String DELIMITER = " ";
+
     private final HttpMethod method;
     private final HttpPath path;
     private final HttpVersion version;
 
     public RequestLine(final String requestLine) {
-        final String[] split = parseRequestLine(requestLine);
-        this.method = HttpMethod.valueOf(split[METHOD_INDEX]);
-        this.path = new HttpPath(split[PATH_INDEX]);
-        this.version = new HttpVersion(split[2]);
+        final String[] parsedRequestLine = parseRequestLine(requestLine);
+        this.method = HttpMethod.valueOf(parsedRequestLine[METHOD_INDEX]);
+        this.path = new HttpPath(parsedRequestLine[PATH_INDEX]);
+        this.version = new HttpVersion(parsedRequestLine[VERSION_INDEX]);
     }
 
     private String[] parseRequestLine(final String requestLine) {
-        final String[] split = requestLine.split(" ");
-        if (split.length != 3) {
+        final String[] splitRequestLine = requestLine.split(DELIMITER);
+        if (splitRequestLine.length != REQUIRED_REQUEST_LINE_LENGTH) {
             throw new InvalidRequestLineException("Invalid request line: " + requestLine);
         }
-        return split;
+        return splitRequestLine;
     }
 
     public HttpMethod getMethod() {
