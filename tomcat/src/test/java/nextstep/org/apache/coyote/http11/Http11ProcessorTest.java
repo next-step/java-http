bbb2d15f3feb07424e3bf884b5bddce3b504fe7e
SeolYoungKim <nasur4da@gmail.com>
2024-07-07 23:24:03 +0900

    test(Http11Processor): 통과하지 못하는 테스트 코드를 목적에 맞게 변경

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -26,11 +26,10 @@ class Http11ProcessorTest {
         var expected = String.join("\r\n",
                 "HTTP/1.1 200 OK ",
                 "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 12 ",
-                "",
-                "Hello world!");
+                "Content-Length: 5564 ",
+                "");
 
-        assertThat(socket.output()).isEqualTo(expected);
+        assertThat(socket.output()).contains(expected);
     }
 
     @Test
