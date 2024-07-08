56a670b495ab337805ea8e2eea6ea7139b315907
jinyoung <jinyoungchoi95@gmail.com>
2024-07-09 02:29:59 +0900

    test: HttpRequestHeaders 정상 파싱 테스트 추가

--- tomcat/src/test/java/camp/nextstep/domain/http/HttpRequestHeadersTest.java
+++ tomcat/src/test/java/camp/nextstep/domain/http/HttpRequestHeadersTest.java
@@ -3,7 +3,9 @@ package camp.nextstep.domain.http;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class HttpRequestHeadersTest {
@@ -20,5 +22,11 @@ class HttpRequestHeadersTest {
         assertThatThrownBy(() -> new HttpRequestHeaders(List.of("Content-Type: ERROR: ERROR")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("RequestHeader값이 정상적으로 입력되지 않았습니다 - Content-Type: ERROR: ERROR");
+    }
+
+    @Test
+    void 헤더값을_파싱하여_생성한다() {
+        HttpRequestHeaders actual = new HttpRequestHeaders(List.of("Content-Length: 55", "Content-Type: A"));
+        assertThat(actual.getHeaders()).containsAllEntriesOf(Map.of("Content-Length", "55", "Content-Type", "A"));
     }
 }
