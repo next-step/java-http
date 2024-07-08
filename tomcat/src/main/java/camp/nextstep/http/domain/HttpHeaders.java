70414c5a9a4cf41d73a67ca27a385ece9930dba1
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-08 12:48:03 +0900

    fix(HttpHeaders): 매직 리터럴 정리

--- tomcat/src/main/java/camp/nextstep/http/domain/HttpHeaders.java
+++ tomcat/src/main/java/camp/nextstep/http/domain/HttpHeaders.java
@@ -7,6 +7,10 @@ import java.util.Map;
 
 public class HttpHeaders {
 
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final long MIN_CONTENT_LENGTH = 1L;
+    private static final String CONTENT_LENGTH = "Content-Length";
+    private static final int EMPTY_CONTENT_LENGTH = 0;
     private final Map<String, String> headers;
 
     public HttpHeaders() {
@@ -14,25 +18,25 @@ public class HttpHeaders {
     }
 
     public void setContentType(final String contentType) {
-        headers.put("Content-Type", contentType);
+        headers.put(CONTENT_TYPE, contentType);
     }
 
     public String getContentType() {
-        return headers.get("Content-Type");
+        return headers.get(CONTENT_TYPE);
     }
 
     public void setContentLength(final long contentLength) {
-        if (contentLength < 1L) {
+        if (contentLength < MIN_CONTENT_LENGTH) {
             throw new InvalidHttpHeaderException("ContentLength must be grater than 0");
         }
 
-        headers.put("Content-Length", String.valueOf(contentLength));
+        headers.put(CONTENT_LENGTH, String.valueOf(contentLength));
     }
 
     public long getContentLength() {
-        final String value = headers.get("Content-Length");
+        final String value = headers.get(CONTENT_LENGTH);
         if (value == null) {
-            return 0;
+            return EMPTY_CONTENT_LENGTH;
         }
         return Long.parseLong(value);
     }
