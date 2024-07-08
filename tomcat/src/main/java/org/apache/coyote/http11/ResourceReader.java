cf92615eaa75a6308c227266eada59e4c984de12
SeolYoungKim <nasur4da@gmail.com>
2024-07-09 00:33:35 +0900

    feat(ResourceReader): path를 읽어서 resource를 읽어주는 객체 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/ResourceReader.java
@@ -0,0 +1,58 @@
+package org.apache.coyote.http11;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class ResourceReader {
+    private static final String BASE_DIRECTORY = "static";
+    private static final String FILE_EXTENSION_SEPARATOR = ".";
+    private static final String HTML_FILE_EXTENSTION = ".html";
+
+    public String read(final String path) {
+        URL resourceUrl = findResourceUrl(path);
+        return findFileFrom(resourceUrl);
+    }
+
+    private URL findResourceUrl(final String path) {
+        String fileExtension = ensureFileExtension(path);
+        String resourcePath = getResourcePath(path, fileExtension);
+
+        return getClass()
+                .getClassLoader()
+                .getResource(BASE_DIRECTORY + resourcePath);
+    }
+
+    private String ensureFileExtension(final String resource) {
+        if (!resource.contains(FILE_EXTENSION_SEPARATOR)) {
+            return HTML_FILE_EXTENSTION;
+        }
+
+        return "";
+    }
+
+    private String getResourcePath(final String path, final String fileExtension) {
+        if (isPathIndex(path)) {
+            return "/index.html";
+        }
+
+        return path + fileExtension;
+    }
+
+    private boolean isPathIndex(final String path) {
+        return path.equals("/");
+    }
+
+    private String findFileFrom(final URL resourceUrl) {
+        if (resourceUrl == null) {
+            throw new IllegalArgumentException("파일을 찾을 수 없습니다.");
+        }
+
+        try {
+            return new String(Files.readAllBytes(new File(resourceUrl.getFile()).toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
