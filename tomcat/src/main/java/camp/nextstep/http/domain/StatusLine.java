a2d287978bcd8c8dfb5a2a5345d90de2a50b5bab
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-08 11:30:31 +0900

    feat(StatusLine): ReasonPhase 추출 기능 추가

--- tomcat/src/main/java/camp/nextstep/http/domain/StatusLine.java
+++ tomcat/src/main/java/camp/nextstep/http/domain/StatusLine.java
@@ -1,15 +1,18 @@
 package camp.nextstep.http.domain;
 
 public class StatusLine {
+    private static final int REQUIRED_STATUS_LINE_LENGTH = 3;
     private static final String DELIMITER = " ";
 
     private final HttpVersion version;
     private final int statusCode;
+    private final String reasonPhase;
 
     public StatusLine(final String statusLine) {
         final String[] parsedStatusLine = statusLine.split(DELIMITER);
         this.version = new HttpVersion(parsedStatusLine[0]);
         this.statusCode = Integer.parseInt(parsedStatusLine[1]);
+        this.reasonPhase = parsedStatusLine[2];
     }
 
     public HttpVersion getVersion() {
@@ -18,5 +21,9 @@ public class StatusLine {
 
     public int getStatusCode() {
         return statusCode;
+    }
+
+    public String getReasonPhase() {
+        return reasonPhase;
     }
 }
--- tomcat/src/test/java/camp/nextstep/http/domain/StatusLineTest.java
+++ tomcat/src/test/java/camp/nextstep/http/domain/StatusLineTest.java
@@ -20,4 +20,10 @@ class StatusLineTest {
         assertThat(statusLine.getStatusCode()).isEqualTo(200);
     }
 
+    @Test
+    void statusLine_에서_Reason_Phase_를_반환받을_수_있다() {
+        final StatusLine statusLine = new StatusLine("HTTP/1.1 200 OK");
+
+        assertThat(statusLine.getReasonPhase()).isEqualTo("OK");
+    }
 }
