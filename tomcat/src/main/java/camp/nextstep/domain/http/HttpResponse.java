2e4d7e441a0bbe7573b0e038e2e28264d5c05464
jinyoung <jinyoungchoi95@gmail.com>
2024-07-08 22:24:45 +0900

    feat: HttpResponse responseBody 저장 시 content length 저장 추가 및 테스트

--- tomcat/src/main/java/camp/nextstep/domain/http/HttpResponse.java
+++ tomcat/src/main/java/camp/nextstep/domain/http/HttpResponse.java
@@ -1,21 +1,27 @@
 package camp.nextstep.domain.http;
 
+import java.util.Map;
+
 public class HttpResponse {
 
     private final HttpProtocol httpProtocol;
     private final HttpStatus httpStatus;
+    private final Map<String, String> httpHeaders;
+    private final String responseBody;
 
-    public HttpResponse(HttpProtocol httpProtocol, HttpStatus httpStatus) {
+    public HttpResponse(HttpProtocol httpProtocol, HttpStatus httpStatus, String responseBody) {
         this.httpProtocol = httpProtocol;
         this.httpStatus = httpStatus;
+        this.httpHeaders = Map.of("Content-Length", String.valueOf(responseBody.getBytes().length));
+        this.responseBody = responseBody;
     }
 
-    public static HttpResponse ok(HttpProtocol httpProtocol) {
-        return new HttpResponse(httpProtocol, HttpStatus.OK);
+    public static HttpResponse ok(HttpProtocol httpProtocol, String responseBody) {
+        return new HttpResponse(httpProtocol, HttpStatus.OK, responseBody);
     }
 
-    public static HttpResponse found(HttpProtocol httpProtocol) {
-        return new HttpResponse(httpProtocol, HttpStatus.FOUND);
+    public static HttpResponse found(HttpProtocol httpProtocol, String responseBody) {
+        return new HttpResponse(httpProtocol, HttpStatus.FOUND, responseBody);
     }
 
     public HttpProtocol getHttpProtocol() {
@@ -24,5 +30,13 @@ public class HttpResponse {
 
     public HttpStatus getHttpStatus() {
         return httpStatus;
+    }
+
+    public Map<String, String> getHttpHeaders() {
+        return httpHeaders;
+    }
+
+    public String getResponseBody() {
+        return responseBody;
     }
 }
--- tomcat/src/test/java/camp/nextstep/domain/http/HttpResponseTest.java
+++ tomcat/src/test/java/camp/nextstep/domain/http/HttpResponseTest.java
@@ -2,19 +2,27 @@ package camp.nextstep.domain.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HttpResponseTest {
 
     @Test
     void ok_response를_생성한다() {
-        HttpResponse actual = HttpResponse.ok(new HttpProtocol("HTTP/1.1"));
+        HttpResponse actual = HttpResponse.ok(new HttpProtocol("HTTP/1.1"), "Hello world!");
         assertThat(actual.getHttpStatus()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
     void found_response를_생성한다() {
-        HttpResponse actual = HttpResponse.found(new HttpProtocol("HTTP/1.1"));
+        HttpResponse actual = HttpResponse.found(new HttpProtocol("HTTP/1.1"), "Hello world!");
         assertThat(actual.getHttpStatus()).isEqualTo(HttpStatus.FOUND);
+    }
+
+    @Test
+    void header에_responseBody의_길이가_저장된다() {
+        HttpResponse actual = new HttpResponse(new HttpProtocol("HTTP/1.1"), HttpStatus.OK, "Hello world!");
+        assertThat(actual.getHttpHeaders()).contains(Map.entry("Content-Length", "12"));
     }
 }
