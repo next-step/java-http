b77c7b31b2c902fd3356eb03f5f8affe9af223fa
JUNSEO <djawnstj44@naver.com>
2024-07-08 16:17:39 +0900

    refactor: Http11Input 내부 상태 변경
    Http11Input 클래스의 내부 상태로 ByteBuffer 를 갖게 하고, RequestLine 을 파싱할 때 해당 버퍼로 바이트를 하나씩 읽던 로직에서
    BufferedReader 를 상태로 갖게 한 후 reader 에서 한줄씩 문자열로 읽는 방법으로 변경하였다.

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Input.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Input.java
@@ -1,48 +1,22 @@
 package org.apache.coyote.http11;
 
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
+import java.io.InputStreamReader;
 
 public class Http11Input {
 
-    private final ByteBuffer byteBuffer;
+    private final BufferedReader reader;
+
     private final Request request = new Request();
 
     public Http11Input(final InputStream inputStream) {
-        this.byteBuffer = toByteBuffer(inputStream);
+        this.reader = new BufferedReader(new InputStreamReader(inputStream));
     }
 
-    public ByteBuffer toByteBuffer(final InputStream inputStream) {
-        try {
-            byte[] byteArray = readInputStreamToByteArray(inputStream);
-            return ByteBuffer.wrap(byteArray);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private byte[] readInputStreamToByteArray(final InputStream inputStream) throws IOException {
-        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        final byte[] temp = new byte[1024];
-
-        int bytesRead = readChunk(inputStream, temp);
-
-        while (bytesRead != -1) {
-            buffer.write(temp, 0, bytesRead);
-            bytesRead = readChunk(inputStream, temp);
-        }
-
-        return buffer.toByteArray();
-    }
-
-    private int readChunk(final InputStream inputStream, final byte[] temp) throws IOException {
-        return inputStream.read(temp);
-    }
-
-    void parseRequestLine() {
-        final String requestLine = readBufferLine();
+    void parseRequestLine() throws IOException {
+        final String requestLine = reader.readLine();
 
         final String[] requestLineMetaData = requestLine.split(" ");
 
@@ -55,34 +29,11 @@ public class Http11Input {
         request.setProtocol(requestLineMetaData[2]);
     }
 
-    private String readBufferLine() {
-        final StringBuilder line = new StringBuilder();
-        boolean endOfLine = false;
-
-        while (byteBuffer.hasRemaining() && !endOfLine) {
-            char c = (char) byteBuffer.get();
-            endOfLine = processChar(line, c);
-        }
-
-        return line.isEmpty() && !byteBuffer.hasRemaining() ? null : line.toString();
-    }
-
-    private boolean processChar(final StringBuilder line, final char c) {
-        if (c == Constants.CR || c == Constants.LF) {
-            handleNewLine(c);
-            return true;
-        }
-        line.append(c);
-        return false;
-    }
-
-    private void handleNewLine(final char c) {
-        if (c == Constants.CR && byteBuffer.hasRemaining() && byteBuffer.get(byteBuffer.position()) == Constants.LF) {
-            byteBuffer.get();
-        }
-    }
-
     public Request getRequest() {
         return this.request;
+    }
+
+    public void close() throws IOException {
+        this.reader.close();
     }
 }
