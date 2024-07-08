0f66bb945620ea0b523e4cd8dedeb1ea95dafe54
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-08 17:33:13 +0900

    refactor(Http11Processor): `StatusLine`, ``HttpHeaders` 를 사용하도록 변경

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,16 +1,14 @@
 package org.apache.coyote.http11;
 
 import camp.nextstep.exception.UncheckedServletException;
-import camp.nextstep.http.domain.HttpPath;
-import camp.nextstep.http.domain.RequestLine;
-import camp.nextstep.http.domain.RequestURI;
+import camp.nextstep.http.domain.*;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -33,42 +31,39 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStreamReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = new BufferedOutputStream(connection.getOutputStream())) {
 
             final RequestLine requestLine = new RequestLine(inputStreamReader.readLine());
-
             final RequestURI requestURI = requestLine.getRequestURI();
-
-            var responseBody = "Hello world!".getBytes();
-            var responseContentType = "text/html";
             final HttpPath path = requestURI.getPath();
-            if (path.isHtml()) {
-                InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("static" + path.getPath());
-                if (inputStream == null) {
-                    inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("static/404.html");
-                }
-                responseBody = inputStream.readAllBytes();
-            } else if (path.isCss()) {
-                InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("static" + path.getPath());
-                responseBody = inputStream.readAllBytes();
-                responseContentType = "text/css";
-            } else if (path.isJs()) {
-                InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("static" + path.getPath());
-                responseBody = inputStream.readAllBytes();
-                responseContentType = "text/javascript";
-            }
+
+            final var responseBody = getResponseBody(path);
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(ContentType.from(path));
+            headers.setContentLength(responseBody.length);
 
             final var responseHeader = String.join(System.lineSeparator(),
-                    "HTTP/1.1 200 OK ",
-                    String.format("Content-Type: %s;charset=utf-8 ", responseContentType),
-                    "Content-Length: " + responseBody.length + " ",
+                    StatusLine.createOk().convertToString(),
+                    headers.convertToString(),
                     System.lineSeparator());
 
             outputStream.write(responseHeader.getBytes());
             outputStream.write(responseBody);
             outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
+        } catch (final IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private byte[] getResponseBody(final HttpPath path) throws IOException {
+        if ("/".equals(path.getPath())) {
+            return "Hello world!".getBytes();
+        }
+
+        final Resource resource = new Resource("static" + path.getPath());
+        if (resource.exists()) {
+            return resource.readAllBytes();
+        }
+        return new Resource("static/404.html").readAllBytes();
     }
 }
