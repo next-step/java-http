b8ab2b7b2b610113ed85082440e4f8d1ca88c368
lsmman <lsmman07@gmail.com>
2024-07-08 22:27:36 +0900

    test: POST Request Parser test 추가

--- tomcat/src/test/java/camp/nextstep/RequestParserTest.java
+++ tomcat/src/test/java/camp/nextstep/RequestParserTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RequestParserTest {
 
     @Test
-    void parseRequest() {
+    void parseGetRequest() {
         // given
         final String httpRequest= String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
@@ -23,6 +23,26 @@ class RequestParserTest {
             RequestLine requestLine = RequestParser.parseRequest(stubInputStream);
             assertEquals(requestLine.getMethod(), "GET");
             assertEquals(requestLine.getPath(), "/index.html");
+            assertEquals(requestLine.getProtocol(), "HTTP");
+            assertEquals(requestLine.getVersion(), "1.1");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void parsePostRequest() {
+        // given
+        final String httpRequest= String.join("\r\n",
+                "POST /user HTTP/1.1 ",
+                "Host: localhost:8080 ",
+                "Connection: keep-alive ",
+                "",
+                "");
+        try(StubInputStream stubInputStream = new StubInputStream(httpRequest)) {
+            RequestLine requestLine = RequestParser.parseRequest(stubInputStream);
+            assertEquals(requestLine.getMethod(), "POST");
+            assertEquals(requestLine.getPath(), "/user");
             assertEquals(requestLine.getProtocol(), "HTTP");
             assertEquals(requestLine.getVersion(), "1.1");
         } catch (IOException e) {
