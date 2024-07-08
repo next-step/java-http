81b5f9f618f906b655831ad7d019a10de5b8cf9a
JUNSEO <djawnstj44@naver.com>
2024-07-08 16:22:53 +0900

    feat: 매핑된 서블릿을 가지고 있는 ServletMapping 개발

--- tomcat/src/main/java/org/apache/catalina/connector/ServletMapping.java
+++ tomcat/src/main/java/org/apache/catalina/connector/ServletMapping.java
@@ -3,7 +3,7 @@ package org.apache.catalina.connector;
 import com.javax.servlet.Servlet;
 import org.apache.catalina.servlets.DefaultServlet;
 import org.apache.coyote.ServletHandler;
-import org.apache.coyote.http.Request;
+import org.apache.coyote.http11.Request;
 
 import java.util.*;
 
@@ -16,7 +16,7 @@ public class ServletMapping {
     private final List<ServletHandler> servletHandlers = new ArrayList<>();
 
     public void addServlet(final String mapping, final Servlet servlet) {
-        this.servletHandlers.add(0, new ServletHandler(mapping, servlet));
+        this.servletHandlers.addFirst(new ServletHandler(mapping, servlet));
     }
 
     public Servlet getServlet(final Request request) {
