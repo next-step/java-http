bfb488143264f57f527c5f790922d15fc7c87332
SeolYoungKim <nasur4da@gmail.com>
2024-07-08 21:49:53 +0900

    feat(RequestTarget): 테스트가 성공하도록 RequestTarget 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestTarget.java
@@ -0,0 +1,36 @@
+package org.apache.coyote.http11;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RequestTarget {
+        private static final String PATH_QUERY_SEPARATOR = "\\?";
+        private static final String QUERY_SEPARATOR = "&";
+        private static final String QUERY_KEY_VALUE_SEPARATOR = "=";
+
+        private final String path;
+        private final Map<String, String> queryParamMap;
+
+        private RequestTarget(final String path, final Map<String, String> queryParamMap) {
+            this.path = path;
+            this.queryParamMap = queryParamMap;
+        }
+
+        public static RequestTarget from(String requestTarget) {
+            String[] pathAndQueries = requestTarget.split(PATH_QUERY_SEPARATOR);
+
+            if (pathAndQueries.length > 1) {
+                String path = pathAndQueries[0];
+                String queries = pathAndQueries[1];
+
+                Map<String, String> queryParamMap = Arrays.stream(queries.split(QUERY_SEPARATOR))
+                        .map(query -> query.split(QUERY_KEY_VALUE_SEPARATOR))
+                        .collect(Collectors.toUnmodifiableMap(query -> query[0], query -> query[1]));
+
+                return new RequestTarget(path, queryParamMap);
+            }
+
+            return new RequestTarget(requestTarget, Map.of());
+        }
+    }
--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/RequestTargetTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/RequestTargetTest.java
@@ -1,5 +1,6 @@
 package nextstep.org.apache.coyote.http11;
 
+import org.apache.coyote.http11.RequestTarget;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,20 +9,6 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RequestTargetTest {
-    static class RequestTarget {
-        private final String path;
-        private final Map<String, String> queryStringMap;
-
-        private RequestTarget(final String path, final Map<String, String> queryStringMap) {
-            this.path = path;
-            this.queryStringMap = queryStringMap;
-        }
-
-        public static RequestTarget from(String requestTarget) {
-            return new RequestTarget("", Map.of());
-        }
-    }
-
     @DisplayName("String requestTarget에 쿼리 스트링이 없을 경우 path만 가진 객체가 생성된다.")
     @Test
     void from() {
@@ -30,7 +17,7 @@ public class RequestTargetTest {
         RequestTarget requestTarget = RequestTarget.from(requestTargetStr);
 
         assertThat(requestTarget).extracting("path").isEqualTo(requestTargetStr);
-        assertThat(requestTarget).extracting("queryStringMap").isEqualTo(Map.of());
+        assertThat(requestTarget).extracting("queryParamMap").isEqualTo(Map.of());
     }
 
     @DisplayName("String requestTarget에 쿼리 스트링이 존재할 경우 path와 queryString을 모두 가진 객체가 생성된다.")
@@ -40,8 +27,8 @@ public class RequestTargetTest {
 
         RequestTarget requestTarget = RequestTarget.from(requestTargetStr);
 
-        assertThat(requestTarget).extracting("path").isEqualTo(requestTargetStr);
-        assertThat(requestTarget).extracting("queryStringMap").isEqualTo(Map.of("name", "kim"));
+        assertThat(requestTarget).extracting("path").isEqualTo("/users");
+        assertThat(requestTarget).extracting("queryParamMap").isEqualTo(Map.of("name", "kim"));
     }
 
     @DisplayName("쿼리 스트링은 &를 기준으로 분리되어 key = value 형태로 저장된다.")
@@ -51,7 +38,7 @@ public class RequestTargetTest {
 
         RequestTarget requestTarget = RequestTarget.from(requestTargetStr);
 
-        assertThat(requestTarget).extracting("queryStringMap").isEqualTo(Map.of(
+        assertThat(requestTarget).extracting("queryParamMap").isEqualTo(Map.of(
                 "name", "kim",
                 "age", "100"
         ));
