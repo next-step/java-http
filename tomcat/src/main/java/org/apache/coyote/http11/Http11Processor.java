a13053120d95afdd4268c135c99fa5efe3b26a12
JUNSEO <djawnstj44@naver.com>
2024-07-08 16:25:07 +0900

    refactor: Http11Processor 요청 처리 로직 역할 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,20 +1,13 @@
 package org.apache.coyote.http11;
 
 import camp.nextstep.exception.UncheckedServletException;
-import jakarta.servlet.ServletContext;
+import org.apache.catalina.connector.CoyoteAdapter;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -22,8 +15,11 @@ public class Http11Processor implements Runnable, Processor {
 
     private final Socket connection;
 
-    public Http11Processor(final Socket connection) {
+    private final CoyoteAdapter adapter;
+
+    public Http11Processor(final Socket connection, final CoyoteAdapter adapter) {
         this.connection = connection;
+        this.adapter = adapter;
     }
 
     @Override
@@ -42,37 +38,18 @@ public class Http11Processor implements Runnable, Processor {
 
             final Request request = http11Input.getRequest();
 
-            var responseBody = "Hello world!";
-            // TODO 일급컬렉션 변경
-            final Map<String, String> responseHeaderMap = new HashMap<>();
-            responseHeaderMap.put("Content-Type", "text/html;charset=utf-8 ");
+            final Response response = new Response();
+            response.init();
 
-            // TODO 역할 분리
-            final URL resource = getClass().getClassLoader().getResource("static/" + request.getPath());
+            adapter.service(request, response);
 
-            if (resource != null) {
-                final Path path = new File(resource.getFile()).toPath();
-                responseBody = new String(Files.readAllBytes(path));
-                final String mimeType = Files.probeContentType(path);
-                responseHeaderMap.put("Content-Type", mimeType + ";charset=utf-8 ");
-            }
-
-            final String headers = responseHeaderMap.entrySet()
-                    .stream()
-                    .map(entry -> entry.getKey() + ": " + entry.getValue())
-                    .collect(Collectors.joining("\r\n"));
-
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    headers,
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
+            outputStream.write(response.toBytes());
             outputStream.flush();
+            http11Input.close();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
