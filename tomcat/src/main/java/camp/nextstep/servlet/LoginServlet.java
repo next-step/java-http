5b7a787f85e14c8efc5fdbb7a0adc96567e86117
JUNSEO <djawnstj44@naver.com>
2024-07-08 16:26:42 +0900

    feat: 미션을 위한 로그인 서블릿 생성

---
+++ tomcat/src/main/java/camp/nextstep/servlet/LoginServlet.java
@@ -0,0 +1,23 @@
+package camp.nextstep.servlet;
+
+import camp.nextstep.model.User;
+import com.javax.servlet.Servlet;
+import org.apache.coyote.http11.Request;
+import org.apache.coyote.http11.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginServlet implements Servlet {
+
+    private final static Logger log = LoggerFactory.getLogger(LoginServlet.class);
+
+    @Override
+    public void service(final Request request, final Response response) throws Exception {
+        response.setStaticResource("login.html");
+        final String account = request.getParameter("account");
+        final String password = request.getParameter("password");
+        final String email = request.getParameter("email");
+
+        log.debug("user: {}", new User(account, password, email));
+    }
+}
