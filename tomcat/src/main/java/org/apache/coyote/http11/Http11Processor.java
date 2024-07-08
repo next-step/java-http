c54951bab4364db408d8b10f1c7b3c089d07cd6c
gutenLee <3bnm1128@naver.com>
2024-07-08 23:54:03 +0900

    test : Http11Processor 일단 성공하는 코드 완성

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,17 @@
 package org.apache.coyote.http11;
 
 import camp.nextstep.exception.UncheckedServletException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -29,6 +34,28 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
+            String requestLine = getRequestLine(inputStream);
+
+            String[] infos = requestLine.split(StringUtils.SPACE);
+            if (infos.length < 3) {
+                throw new UncheckedServletException(new RuntimeException("Request is invalid"));
+            }
+
+            final var url = infos[1];
+
+            if (url.endsWith("/index.html")) {
+                final URL resource = getClass().getClassLoader().getResource("static/index.html");
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: 5564 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
+                        "",
+                        new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                return;
+            }
+
             final var responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
@@ -43,5 +70,15 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+
+    public String getRequestLine(InputStream is) {
+        final var br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+        final var lines = br.lines().toList();
+        if (!lines.isEmpty()) {
+            return lines.get(0);
+        }
+        throw new UncheckedServletException(new RuntimeException("Request is empty"));
     }
 }
