cec4181696c24a8923473f9bdddd5d96a8d0eff3
jinyoung <jinyoungchoi95@gmail.com>
2024-07-09 20:57:42 +0900

    feat: session attribute 관리기능 추가 및 테스트

--- tomcat/src/main/java/camp/nextstep/domain/session/Session.java
+++ tomcat/src/main/java/camp/nextstep/domain/session/Session.java
@@ -1,11 +1,14 @@
 package camp.nextstep.domain.session;
 
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Session {
 
     private final String id;
+    private final Map<String, Object> values = new ConcurrentHashMap<>();
 
     public Session(String id) {
         this.id = id;
@@ -13,6 +16,17 @@ public class Session {
 
     public static Session createNewSession() {
         return new Session(String.valueOf(UUID.randomUUID()));
+    }
+
+    public Object getAttribute(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("존재하지 않는 세션의 정보입니다. - " + key);
+        }
+        return values.get(key);
+    }
+
+    public void setAttribute(String key, Object value) {
+        values.put(key, value);
     }
 
     public String getId() {
