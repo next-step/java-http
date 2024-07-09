87af696acd3c3b443bc0f0062b8e046757eefb86
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-10 03:34:52 +0900

    test(HttpRequestTest): getSession 테스트 추가

--- tomcat/src/test/java/camp/nextstep/http/domain/HttpRequestTest.java
+++ tomcat/src/test/java/camp/nextstep/http/domain/HttpRequestTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 class HttpRequestTest {
@@ -50,5 +51,23 @@ class HttpRequestTest {
             softly.assertThat(httpRequest.getHeaders()).isEqualTo(new HttpHeaders(List.of("Host: localhost:8080", "Connection: keep-alive", "Content-Length: 50")));
             softly.assertThat(httpRequest.getRequestBody()).isEqualTo(new RequestBody("account=test&password=password&email=test@test.com"));
         });
+    }
+
+    @Test
+    @DisplayName("Http 요청에 Session 정보를 가져올 수 있다.")
+    void httpRequest_session_test() throws IOException {
+        final HttpSession existingSession = new HttpSession("session");
+        HttpSessionManager.add(existingSession);
+        final String httpRequestString = "GET /index.html HTTP/1.1 \r\n" +
+                "Host: localhost:8080 \r\n" +
+                "Connection: keep-alive \r\n" +
+                "Content-Length: 50 \r\n" +
+                "Cookie: JSESSIONID=session \r\n" +
+                "\r\n";
+        final InputStream inputStream = new ByteArrayInputStream(httpRequestString.getBytes());
+
+        final HttpRequest httpRequest = new HttpRequest(inputStream);
+
+        assertThat(httpRequest.getSession(false)).isEqualTo(new HttpSession("session"));
     }
 }
