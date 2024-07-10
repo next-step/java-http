ee6091a677b488b20d8d405b2520be8c3a28bca3
woo-yu <hjwoou@gmail.com>
2024-07-10 22:32:09 +0900

    [refactor] HttpParams toString

--- tomcat/src/main/java/org/apache/http/HttpParams.java
+++ tomcat/src/main/java/org/apache/http/HttpParams.java
@@ -3,9 +3,12 @@ package org.apache.http;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class HttpParams {
+    private static final String PARAM_DELIMITER = "&";
+    private static final String KEY_VALUE_DELIMITER = "=";
+
     private Map<String, String> params = new HashMap<>();
 
     HttpParams() {
@@ -16,10 +19,7 @@ public class HttpParams {
     }
 
     public HttpParams(final String url) {
-        if (!url.contains("?")) {
-            return;
-        }
-        parseQueryString(url.split("\\?")[1]);
+        parseQueryString(url);
     }
 
     public String get(final String key) {
@@ -27,9 +27,9 @@ public class HttpParams {
     }
 
     private void parseQueryString(final String queryString) {
-        final var values = queryString.split("&");
+        final var values = queryString.split(PARAM_DELIMITER);
         for (String param : values) {
-            var token = param.split("=");
+            var token = param.split(KEY_VALUE_DELIMITER);
             if (token.length < 2) continue;
             final String key = token[0];
             final String value = token[1];
@@ -48,5 +48,12 @@ public class HttpParams {
     @Override
     public int hashCode() {
         return Objects.hash(params);
+    }
+
+    @Override
+    public String toString() {
+        return params.entrySet().stream()
+                .map(param -> param.getKey() + KEY_VALUE_DELIMITER + param.getValue())
+                .collect(Collectors.joining(PARAM_DELIMITER));
     }
 }
