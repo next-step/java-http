bad11a4254c8ec461a76879b0cf6f0c334696ccd
jeongho <qkr2435@everon.co.kr>
2024-07-11 08:48:25 +0900

    feat: requirement 1 GET 요청

--- tomcat/src/main/java/camp/nextstep/http/RequestLine.java
+++ tomcat/src/main/java/camp/nextstep/http/RequestLine.java
@@ -2,4 +2,43 @@ package camp.nextstep.http;
 
 public class RequestLine {
 
+  private final String httpMethod;
+  private final String path;
+  private final String protocol;
+  private final String version;
+
+  public String getHttpMethod() {
+    return httpMethod;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public String getProtocol() {
+    return protocol;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public static RequestLine parse(String request) {
+    final String[] tokens = request.split(" ");
+    final String httpMethod = tokens[0];
+    final String path = tokens[1];
+
+    final String[] versionTokens = tokens[2].split("/");
+    final String protocol = versionTokens[0];
+    final String version = versionTokens[1];
+
+    return new RequestLine(httpMethod, path, protocol, version);
+  }
+
+  public RequestLine(String httpMethod, String path, String protocol, String version) {
+    this.httpMethod = httpMethod;
+    this.path = path;
+    this.protocol = protocol;
+    this.version = version;
+  }
 }
--- tomcat/src/test/java/camp/nextstep/http/RequestLineTest.java
+++ tomcat/src/test/java/camp/nextstep/http/RequestLineTest.java
@@ -1,7 +1,28 @@
 package camp.nextstep.http;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class RequestLineTest {
+
+  @Test
+  @DisplayName(" HTTP GET 요청에 대한 RequestLine을 파싱한다.")
+  void parseHttpGetRequest() {
+    // given
+    final String request = "GET /users HTTP/1.1";
+
+    // when
+    final RequestLine requestLine = RequestLine.parse(request);
+
+    // then
+    assertThat(requestLine.getHttpMethod()).isEqualTo("GET");
+    assertThat(requestLine.getPath()).isEqualTo("/users");
+    assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
+    assertThat(requestLine.getVersion()).isEqualTo("1.1");
+
+  }
+
 
 }
