4f559a2164a132395503e32c47e802997d0c8afc
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-11 03:06:54 +0900

    fix(LoginController): 코드 정리

--- tomcat/src/main/java/camp/nextstep/http/domain/LoginController.java
+++ tomcat/src/main/java/camp/nextstep/http/domain/LoginController.java
@@ -13,17 +13,20 @@ public class LoginController extends AbstractController {
     private static final String SESSION_USER_KEY = "user";
     private static final String ACCOUNT = "account";
     private static final String PASSWORD = "password";
+    private static final String INDEX_PATH = "/index.html";
+    private static final String LOGIN_PATH = "/login.html";
+    private static final String UNAUTHORIZED_PATH = "/401.html";
 
     @Override
     protected void doGet(final HttpRequest request, final HttpResponse response) throws IOException {
         final HttpSession session = request.getSession(false);
         if (session != null && session.getAttribute(SESSION_USER_KEY) != null) {
-            response.sendRedirect("/index.html");
+            response.sendRedirect(INDEX_PATH);
             return;
         }
 
         response.setContentType(ContentType.HTML);
-        response.forward("/login.html");
+        response.forward(LOGIN_PATH);
     }
 
     @Override
@@ -32,19 +35,23 @@ public class LoginController extends AbstractController {
         final Optional<User> userOptional = InMemoryUserRepository.findByAccount(requestBody.get(ACCOUNT));
 
         if (userOptional.isEmpty()) {
-            response.sendRedirect("/401.html");
+            response.sendRedirect(UNAUTHORIZED_PATH);
             return;
         }
 
         final User user = userOptional.get();
         if (user.checkPassword(requestBody.get(PASSWORD))) {
-            log.info("user : {}", user);
-            final HttpSession httpSession = request.getSession(true);
-            httpSession.setAttribute(SESSION_USER_KEY, user);
-            response.setSession(httpSession.getId());
-            response.sendRedirect("/index.html");
+            processLogin(request, response, user);
             return;
         }
-        response.sendRedirect("/401.html");
+        response.sendRedirect(UNAUTHORIZED_PATH);
+    }
+
+    private void processLogin(final HttpRequest request, final HttpResponse response, final User user) throws IOException {
+        log.info("user : {}", user);
+        final HttpSession httpSession = request.getSession(true);
+        httpSession.setAttribute(SESSION_USER_KEY, user);
+        response.setSession(httpSession.getId());
+        response.sendRedirect(INDEX_PATH);
     }
 }
