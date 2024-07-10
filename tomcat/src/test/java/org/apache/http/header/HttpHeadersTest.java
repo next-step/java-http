133b0a2c9846a281cf35f96e9307c32e3963f2f9
woo-yu <hjwoou@gmail.com>
2024-07-10 20:05:41 +0900

    [test] headers 테스트 추가

--- tomcat/src/test/java/org/apache/http/header/HttpHeadersTest.java
+++ tomcat/src/test/java/org/apache/http/header/HttpHeadersTest.java
@@ -1,7 +1,12 @@
 package org.apache.http.header;
 
+import org.apache.file.MediaType;
+import org.apache.http.body.HttpFormBody;
+import org.apache.http.body.HttpTextBody;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class HttpHeadersTest {
 
@@ -16,6 +21,36 @@ class HttpHeadersTest {
                 .add(new ContentType(MediaType.TEXT_HTML));
 
         Assertions.assertThat(headers.toString()).isEqualTo(reverseHeaders.toString());
+    }
+
+    @Test
+    void messages_to_headers() {
+        var messages = List.of("Content-Type: application/x-www-form-urlencoded;charset=utf-8");
+
+        var actual = new HttpHeaders(messages);
+
+        var expected = new HttpHeaders().add(new ContentType(MediaType.FORM_URL_ENCODED));
+        Assertions.assertThat(actual).hasToString(expected.toString());
+    }
+
+    @Test
+    void messages_parse_body_form() {
+        var headers = new HttpHeaders().add(new ContentType(MediaType.FORM_URL_ENCODED));
+        var messages = "test=hi";
+
+        var actual = headers.parseBody(messages);
+
+        Assertions.assertThat(actual).isInstanceOf(HttpFormBody.class);
+    }
+
+    @Test
+    void messages_parse_body_text() {
+        var headers = new HttpHeaders().add(new ContentType(MediaType.TEXT_HTML));
+        var messages = "test=hi";
+
+        var actual = headers.parseBody(messages);
+
+        Assertions.assertThat(actual).isInstanceOf(HttpTextBody.class);
     }
 
 }
