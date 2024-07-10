acfcb651bff20a73bd1aa0638efdd35e89a3c442
woo-yu <hjwoou@gmail.com>
2024-07-10 20:08:13 +0900

    [fix] mediatype 패키지 이동 반영

--- tomcat/src/main/java/org/apache/http/body/HttpTextBody.java
+++ tomcat/src/main/java/org/apache/http/body/HttpTextBody.java
@@ -1,9 +1,9 @@
 package org.apache.http.body;
 
+import org.apache.file.MediaType;
 import org.apache.http.header.ContentLength;
 import org.apache.http.header.ContentType;
 import org.apache.http.header.HttpHeaders;
-import org.apache.http.header.MediaType;
 
 public class HttpTextBody extends HttpBody {
     private final String body;
