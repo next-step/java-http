2bebfe2e31ab1ce32b22df37caa0dd2fa8e3f86d
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-11 22:21:47 +0900

    fix(HttpRequest): 메서드명 명확하게 변경 및 책임 부여

--- tomcat/src/main/java/camp/nextstep/http/domain/HttpRequest.java
+++ tomcat/src/main/java/camp/nextstep/http/domain/HttpRequest.java
@@ -1,13 +1,20 @@
 package camp.nextstep.http.domain;
 
+import camp.nextstep.http.exception.InvalidHttpHeaderException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class HttpRequest {
+
+    private static final int HEADER_KEY_INDEX = 0;
+    private static final int HEADER_VALUE_INDEX = 1;
+    private static final int SPLIT_LIMIT = 2;
 
     private final RequestLine requestLine;
     private final HttpHeaders headers;
@@ -16,12 +23,30 @@ public class HttpRequest {
     public HttpRequest(final InputStream inputStream) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         this.requestLine = new RequestLine(br.readLine());
-        this.headers = new HttpHeaders(extractHeaders(br));
-        this.requestBody = extractRequestBody(br, headers);
-
+        this.headers = readHeader(br);
+        this.requestBody = readRequestBody(br, headers);
     }
 
-    private RequestBody extractRequestBody(final BufferedReader br, final HttpHeaders requestHeaders) throws IOException {
+    private HttpHeaders readHeader(final BufferedReader br) {
+        return new HttpHeaders(br.lines()
+                .takeWhile(line -> !line.isEmpty())
+                .map(this::splitHeader)
+                .collect(Collectors.toMap(
+                        header1 -> header1[HEADER_KEY_INDEX].trim(),
+                        header1 -> header1[HEADER_VALUE_INDEX].trim(),
+                        (existing, replacement) -> existing,
+                        LinkedHashMap::new
+                )));
+    }
+
+    private String[] splitHeader(final String headerLine) {
+        if (headerLine.contains(HttpHeaders.DELIMITER)) {
+            return headerLine.split(HttpHeaders.DELIMITER, SPLIT_LIMIT);
+        }
+        throw new InvalidHttpHeaderException("Invalid HTTP header line: " + headerLine);
+    }
+
+    private RequestBody readRequestBody(final BufferedReader br, final HttpHeaders requestHeaders) throws IOException {
         if (requestHeaders.isContentLengthEmpty()) {
             return new RequestBody();
         }
@@ -29,12 +54,6 @@ public class HttpRequest {
         final char[] bodyChars = new char[contentLength];
         br.read(bodyChars, 0, contentLength);
         return new RequestBody(new String(bodyChars));
-    }
-
-    private static List<String> extractHeaders(final BufferedReader br) {
-        return br.lines()
-                .takeWhile(line -> !line.isEmpty())
-                .toList();
     }
 
     public HttpPath getPath() {
