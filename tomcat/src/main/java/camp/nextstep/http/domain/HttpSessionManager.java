94961fbf453ca75ca70cc72b7725bc9c7f17cfb3
Jongmin Yoon <jongmin4943@gmail.com>
2024-07-11 23:45:15 +0900

    fix(HttpSessionManager): findSession -> computeIfAbsent 로 변경

--- tomcat/src/main/java/camp/nextstep/http/domain/HttpSessionManager.java
+++ tomcat/src/main/java/camp/nextstep/http/domain/HttpSessionManager.java
@@ -1,6 +1,8 @@
 package camp.nextstep.http.domain;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpSessionManager {
@@ -13,11 +15,11 @@ public class HttpSessionManager {
         SESSIONS.put(session.getId(), session);
     }
 
-    public static HttpSession findSession(final String id) {
-        if (id == null) {
-            return null;
-        }
-        return SESSIONS.get(id);
+    public static HttpSession computeIfAbsent(final String id) {
+        final String sessionId = Optional.ofNullable(id)
+                .orElseGet(() -> UUID.randomUUID().toString());
+
+        return SESSIONS.computeIfAbsent(sessionId, HttpSession::new);
     }
 
     public static void remove(final HttpSession session) {
--- tomcat/src/test/java/camp/nextstep/http/domain/HttpSessionManagerTest.java
+++ tomcat/src/test/java/camp/nextstep/http/domain/HttpSessionManagerTest.java
@@ -16,6 +16,14 @@ class HttpSessionManagerTest {
     }
 
     @Test
+    @DisplayName("HttpSessionManager 에 세션을 가져올때 없으면 생성하고 가져올 수 있다")
+    void computeIfAbsentSessionTest() {
+        HttpSessionManager.computeIfAbsent("test");
+
+        assertThat(HttpSessionManager.get("test").get()).isEqualTo(new HttpSession("test"));
+    }
+
+    @Test
     @DisplayName("HttpSessionManager 에 세션을 제거할 수 있다")
     void removeSessionTest() {
         HttpSessionManager.add(new HttpSession("user"));
