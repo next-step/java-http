2806a566996c9b541949f034274138f22f7bd237
JUNSEO <djawnstj44@naver.com>
2024-07-12 08:52:40 +0900

    refactor: 파일을 못 찾아 예외가 발생하는 문제 해결

--- tomcat/src/main/java/org/apache/coyote/view/StaticResourceNotFoundException.java
+++ tomcat/src/main/java/org/apache/coyote/view/StaticResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package org.apache.coyote.view;
 
 public class StaticResourceNotFoundException extends RuntimeException {
+
+    public StaticResourceNotFoundException(final String message) {
+        super(message);
+    }
 }
--- tomcat/src/main/java/org/apache/coyote/view/StaticResourceResolver.java
+++ tomcat/src/main/java/org/apache/coyote/view/StaticResourceResolver.java
@@ -7,23 +7,41 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 public class StaticResourceResolver {
 
-    private static final String STATIC_RESOURCE_PATH = "static/";
+    private static final String STATIC_RESOURCE_PATH = "static";
 
     public static StaticResource findStaticResource(final String filePath) throws IOException {
-        final URL resource = StaticResourceResolver.class.getClassLoader().getResource(STATIC_RESOURCE_PATH + filePath);
-        final File file = new File(resource.getFile());
+        final URL resource = findResource(filePath);
 
-        if (!file.isFile()) {
-            throw new StaticResourceNotFoundException();
-        }
+        final File file = getFile(resource);
 
         final Path path = file.toPath();
         final String content = new String(Files.readAllBytes(path));
         final String mimeType = Files.probeContentType(path);
 
         return new StaticResource(content, ContentType.from(mimeType));
+    }
+
+    private static URL findResource(final String filePath) {
+        final URL resource = StaticResourceResolver.class.getClassLoader().getResource(STATIC_RESOURCE_PATH + filePath);
+
+        if (Objects.isNull(resource)) {
+            throw new StaticResourceNotFoundException("Not exist resource - " + filePath);
+        }
+
+        return resource;
+    }
+
+    private static File getFile(final URL resource) {
+        final File file = new File(resource.getFile());
+
+        if (!file.isFile()) {
+            throw new StaticResourceNotFoundException("Could not find resource - " + file.getPath());
+        }
+
+        return file;
     }
 }
