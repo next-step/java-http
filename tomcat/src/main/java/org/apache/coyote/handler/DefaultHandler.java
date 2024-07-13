a074f23ce9696b59284ab97f64d207ee4d05b9df
woo-yu <hjwoou@gmail.com>
2024-07-13 17:43:32 +0900

    [refactor] hello world static으로 변경

--- tomcat/src/main/java/org/apache/coyote/handler/DefaultHandler.java
+++ tomcat/src/main/java/org/apache/coyote/handler/DefaultHandler.java
@@ -4,10 +4,11 @@ import org.apache.coyote.*;
 import org.apache.http.body.HttpTextBody;
 
 public class DefaultHandler implements Handler {
+    private static final String MESSAGE = "Hello world!";
 
     @Override
     public HttpResponse handle(HttpRequest request) {
-        final var responseBody = new HttpTextBody("Hello world!");
+        final var responseBody = new HttpTextBody(MESSAGE);
 
         return new HttpResponse(responseBody);
     }
