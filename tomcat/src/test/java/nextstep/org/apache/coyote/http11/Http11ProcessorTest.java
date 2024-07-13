296f1d8fc32d3af88a4f0b35be11150472a3824e
jeongho <qkr2435@everon.co.kr>
2024-07-14 03:17:28 +0900

    test: request이 Null일때 예외처리 test 구현

--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -1,63 +1,80 @@
 package nextstep.org.apache.coyote.http11;
 
-import support.StubSocket;
-import org.apache.coyote.http11.Http11Processor;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
+import camp.nextstep.exception.InvalidRequestException;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.apache.coyote.http11.Http11Processor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import support.StubSocket;
 
 class Http11ProcessorTest {
 
-    @Test
-    void process() {
-        // given
-        final var socket = new StubSocket();
-        final var processor = new Http11Processor(socket);
+  @Test
+  void process() {
+    // given
+    final var socket = new StubSocket();
+    final var processor = new Http11Processor(socket);
 
-        // when
-        processor.process(socket);
+    // when
+    processor.process(socket);
 
-        // then
-        var expected = String.join("\r\n",
-                "HTTP/1.1 200 OK ",
-                "Content-Type: text/html;charset=utf-8 ",
-                "Content-Length: 12 ",
-                "",
-                "Hello world!");
+    // then
+    var expected = String.join("\r\n",
+        "HTTP/1.1 200 OK ",
+        "Content-Type: text/html;charset=utf-8 ",
+        "Content-Length: 12 ",
+        "",
+        "Hello world!");
 
-        assertThat(socket.output()).isEqualTo(expected);
-    }
+    assertThat(socket.output()).isEqualTo(expected);
+  }
 
-    @Test
-    void index() throws IOException {
-        // given
-        final String httpRequest= String.join("\r\n",
-                "GET /index.html HTTP/1.1 ",
-                "Host: localhost:8080 ",
-                "Connection: keep-alive ",
-                "",
-                "");
+  @Test
+  void index() throws IOException {
+    // given
+    final String httpRequest = String.join("\r\n",
+        "GET /index.html HTTP/1.1 ",
+        "Host: localhost:8080 ",
+        "Connection: keep-alive ",
+        "",
+        "");
 
-        final var socket = new StubSocket(httpRequest);
-        final Http11Processor processor = new Http11Processor(socket);
+    final var socket = new StubSocket(httpRequest);
+    final Http11Processor processor = new Http11Processor(socket);
 
-        // when
-        processor.process(socket);
+    // when
+    processor.process(socket);
 
-        // then
-        final URL resource = getClass().getClassLoader().getResource("static/index.html");
-        var expected = String.join("\r\n",
-            "HTTP/1.1 200 OK ",
-            "Content-Type: text/html;charset=utf-8 ",
-            "Content-Length: 5564 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
-            "",
-            new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
+    // then
+    final URL resource = getClass().getClassLoader().getResource("static/index.html");
+    var expected = String.join("\r\n",
+        "HTTP/1.1 200 OK ",
+        "Content-Type: text/html;charset=utf-8 ",
+        "Content-Length: 5564 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
+        "",
+        new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
 
-        assertThat(socket.output()).isEqualTo(expected);
-    }
+    assertThat(socket.output()).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName(" HTTP 요청이 빈 값일때 InvalidRequestException 에러를 리턴한다.")
+  void requestNull() {
+    // given
+    String request = "";
+    final var socket = new StubSocket(request);
+    final var processor = new Http11Processor(socket);
+
+    // when
+    Throwable thrown = catchThrowable(() -> processor.process(socket));
+
+    // then
+    assertThat(thrown).isInstanceOf(InvalidRequestException.class);
+  }
 }
