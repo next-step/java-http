0cecba82c708c9ae8865d698e94a82f9d1e3c46f
woo-yu <hjwoou@gmail.com>
2024-07-13 17:37:53 +0900

    [refactor] 삼항연산자를 통한 parseParam 메소드 분리

--- tomcat/src/main/java/org/apache/coyote/HttpRequestLine.java
+++ tomcat/src/main/java/org/apache/coyote/HttpRequestLine.java
@@ -21,11 +21,18 @@ public class HttpRequestLine {
             this.method = HttpMethod.valueOf(tokens[0]);
             var urlTokens = tokens[1].split(URL_REGEX);
             this.path = new HttpPath(urlTokens[0]);
-            this.params = (urlTokens.length > 1) ? new HttpParams(urlTokens[1]) : null;
+            this.params = parseParam(urlTokens);
             this.protocol = new HttpProtocol(tokens[2]);
         } catch (Exception e) {
             throw new NotSupportHttpRequestException();
         }
+    }
+
+    private HttpParams parseParam(String[] urlTokens) {
+        if (urlTokens.length <= 1) {
+            return null;
+        }
+        return new HttpParams(urlTokens[1]);
     }
 
     @Override
