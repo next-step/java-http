3c4307f53cd7a6838ddd54ee46c5c8b5c02b7fcd
SeolYoungKim <nasur4da@gmail.com>
2024-07-14 11:10:21 +0900

    refactor(ResourceFinder): 비즈니스 로직에 가깝다고 생각되는 부분들을 제거

--- tomcat/src/main/java/org/apache/coyote/http11/ResourceFinder.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ResourceFinder.java
@@ -5,8 +5,6 @@ import java.net.URL;
 
 public class ResourceFinder {
     private static final String BASE_DIRECTORY = "static";
-    private static final String FILE_EXTENSION_SEPARATOR = ".";
-    private static final String HTML_FILE_EXTENSION = ".html";
 
     public File findByPath(final String path) {
         URL resourceUrl = findResourceUrl(path);
@@ -15,32 +13,9 @@ public class ResourceFinder {
     }
 
     private URL findResourceUrl(final String path) {
-        String fileExtension = ensureFileExtension(path);
-        String resourcePath = getResourcePath(path, fileExtension);
-
         return getClass()
                 .getClassLoader()
-                .getResource(BASE_DIRECTORY + resourcePath);
-    }
-
-    private String ensureFileExtension(final String resource) {
-        if (!resource.contains(FILE_EXTENSION_SEPARATOR)) {
-            return HTML_FILE_EXTENSION;
-        }
-
-        return "";
-    }
-
-    private String getResourcePath(final String path, final String fileExtension) {
-        if (isPathIndex(path)) {
-            return "/index.html";
-        }
-
-        return path + fileExtension;
-    }
-
-    private boolean isPathIndex(final String path) {
-        return path.equals("/");
+                .getResource(BASE_DIRECTORY + path);
     }
 
     private void validate(final URL resourceUrl) {
