89b8f089e96f6cebd702c6908bc85f71975fb1d6
SeolYoungKim <nasur4da@gmail.com>
2024-07-14 22:54:02 +0900

    refactor(Common): 사용되지 않는 클래스 제거

--- tomcat/src/main/java/org/apache/coyote/http11/InvalidCookieNameException.java
+++
@@ -1,7 +0,0 @@
-package org.apache.coyote.http11;
-
-public class InvalidCookieNameException extends IllegalArgumentException {
-    public InvalidCookieNameException() {
-        super("Cookie의 이름은 null일 수 없습니다.");
-    }
-}
