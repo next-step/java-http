a51cd4d2bd5c4f99c38b746ab99ebdd85df6a3a3
Hyunta <mohai2618@gmail.com>
2024-07-14 16:30:07 +0900

    학습테스트: HTTP 활용하기

--- study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
+++ study/src/main/java/cache/com/example/cachecontrol/CacheWebConfig.java
@@ -1,6 +1,10 @@
 package cache.com.example.cachecontrol;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +13,19 @@ public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new CacheControlInterceptor());
+    }
+
+    private static class CacheControlInterceptor implements HandlerInterceptor {
+
+        @Override
+        public void postHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final ModelAndView modelAndView) {
+            response.setHeader("Cache-Control", "no-cache, private");
+        }
+
+        @Override
+        public void afterCompletion(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final Exception ex) {
+            // nothing
+        }
     }
 }
