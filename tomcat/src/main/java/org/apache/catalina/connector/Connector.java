fa9314dbe4a8352384ef6b2087052b89c9f012fd
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-07-14 22:26:03 +0900

    🚀 5단계 - 동시성 확장하기 - sessions에는 이미 적용되어 있어 thread pool만 설정

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,13 +1,16 @@
 package org.apache.catalina.connector;
 
-import org.apache.coyote.http11.Http11Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.coyote.http11.Http11Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector implements Runnable {
 
@@ -15,17 +18,31 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    private static final int DEFAULT_POOL_SIZE = 10;
+    private static final int DEFAULT_MAX_THREAD_SIZE = 250;
+    private static final int KEEP_ALIVE_TIME = 60;
+
 
     private final ServerSocket serverSocket;
+    private final ExecutorService threadPool;
+
     private boolean stopped;
 
     public Connector() {
-        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT);
+        this(DEFAULT_PORT, DEFAULT_ACCEPT_COUNT, DEFAULT_POOL_SIZE, DEFAULT_MAX_THREAD_SIZE, KEEP_ALIVE_TIME);
     }
 
-    public Connector(final int port, final int acceptCount) {
+    public Connector(final int port, final int acceptCount, final int poolSize, final int maxThreadSize, int keepAliveTime) {
         this.serverSocket = createServerSocket(port, acceptCount);
         this.stopped = false;
+        this.threadPool = new ThreadPoolExecutor(
+            poolSize,
+            maxThreadSize,
+            keepAliveTime,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(acceptCount),
+            new ThreadPoolExecutor.AbortPolicy()
+        );
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -67,7 +84,7 @@ public class Connector implements Runnable {
             return;
         }
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        threadPool.execute(processor);
     }
 
     public void stop() {
