e323b02e337ecc9f49fb349b018b962aa1817d2c
Dongyoon Lee <nine.cow@gmail.com>
2024-07-14 23:38:42 +0900

    fix: 잘못된 로거 선언 수정

--- tomcat/src/main/java/camp/nextstep/controller/IndexController.java
+++ tomcat/src/main/java/camp/nextstep/controller/IndexController.java
@@ -2,14 +2,13 @@ package camp.nextstep.controller;
 
 import camp.nextstep.request.HttpRequest;
 import camp.nextstep.response.HttpResponse;
-import org.apache.coyote.http11.Http11Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class IndexController extends AbstractController {
-    private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
 
     @Override
     protected void doGet(HttpRequest request, HttpResponse response) throws IOException {
