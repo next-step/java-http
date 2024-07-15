cae2f18e0651b2168a9280b026e4450b13bd5226
woo-yu <hjwoou@gmail.com>
2024-07-15 17:37:10 +0900

    [fix] default 설정 주입을 ControllerConfig에서

--- tomcat/src/main/java/camp/nextstep/config/ControllerConfig.java
+++ tomcat/src/main/java/camp/nextstep/config/ControllerConfig.java
@@ -3,13 +3,16 @@ package camp.nextstep.config;
 import camp.nextstep.controller.DefaultController;
 import camp.nextstep.controller.LoginController;
 import camp.nextstep.controller.RegisterController;
+import org.apache.coyote.RequestMapping;
 
 import java.util.List;
 
 public class ControllerConfig {
 
     public static void initialize() {
-        List.of(new RegisterController(), new LoginController(), new DefaultController());
+        List.of(new RegisterController(), new LoginController());
+        var defaultController = new DefaultController();
+        RequestMapping.addDefault(defaultController);
     }
 
 }
