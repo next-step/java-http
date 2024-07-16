b4ce74f82ab56e401fa9840d9d6f438d32babc9a
SeolYoungKim <nasur4da@gmail.com>
2024-07-16 20:34:14 +0900

    refactor(RequestHandler): handle() 메서드 리팩터링 - 예외 처리 catch문을 추가

--- tomcat/src/main/java/org/apache/coyote/http11/RequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/RequestHandler.java
@@ -2,6 +2,7 @@ package org.apache.coyote.http11;
 
 public class RequestHandler {
     private static final String NOT_FOUND_PATH = "/404.html";
+    private static final String INTERNAL_SERVER_ERROR_PATH = "/500.html";
 
     private final RequestHandlerMapping requestHandlerMapping;
 
@@ -11,30 +12,23 @@ public class RequestHandler {
 
     void handle(final HttpRequest httpRequest, final HttpResponse httpResponse) {
         String path = httpRequest.getPath();
-        if (requestHandlerMapping.isNotRegisteredPath(path)) {
-            handleNotRegisteredRequest(path, httpResponse);
-            return;
-        }
-
-        handleRegisteredRequest(httpRequest, httpResponse, path);
-    }
-
-    private void handleNotRegisteredRequest(final String path, final HttpResponse httpResponse) {
         try {
-            httpResponse.sendResource(path);
-        } catch (ResourceNotFoundException e) {
-            httpResponse.sendRedirect(NOT_FOUND_PATH);
-        }
-    }
-
-    private void handleRegisteredRequest(final HttpRequest httpRequest, final HttpResponse httpResponse, final String path) {
-        Controller controller = requestHandlerMapping.getController(path);
-        try {
-            controller.service(httpRequest, httpResponse);
+            handleRequest(httpRequest, httpResponse, path);
         } catch (ResourceNotFoundException e) {
             httpResponse.sendRedirect(NOT_FOUND_PATH);
         } catch (Exception e) {
-            throw new RuntimeException(e);  // TODO: ExceptionHandler -> 예외에 따라서 처리? 아니면 status? 사용자가 예외를 커스터마이징 할 수 있는 방법?
+            httpResponse.sendRedirect(INTERNAL_SERVER_ERROR_PATH);
+            throw new RuntimeException(e);
         }
+    }
+
+    private void handleRequest(final HttpRequest httpRequest, final HttpResponse httpResponse, final String path) throws Exception {
+        if (requestHandlerMapping.isNotRegisteredPath(path)) {
+            httpResponse.sendResource(path);
+            return;
+        }
+
+        Controller controller = requestHandlerMapping.getController(path);
+        controller.service(httpRequest, httpResponse);
     }
 }
