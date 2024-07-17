16b4d85d526e886f0a3db39beef83368e9779e31
gutenLee <3bnm1128@naver.com>
2024-07-17 16:53:29 +0900

    feat: HttpCookie 모델 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpCookie.java
@@ -0,0 +1,41 @@
+package org.apache.coyote.http11;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class HttpCookie {
+
+    private static final String COOKIE_DELIMITER = "; ";
+    public static final String JSESSIONID = "JSESSIONID";
+    public static final String KEY_VALUE_DELIMITER = "=";
+
+    private Map<String, String> cookies = new HashMap<>();
+
+    public HttpCookie() {
+    }
+
+    public HttpCookie(String line) {
+        this.cookies = toMap(line);
+    }
+
+    private Map<String, String> toMap(String line) {
+        return Arrays.stream(line.split(COOKIE_DELIMITER))
+                .map(cookie -> cookie.split("="))
+                .map(cookie -> Map.entry(cookie[0], cookie[1]))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public void addSessionId(UUID uuid) {
+        cookies.put(JSESSIONID, uuid.toString());
+    }
+
+    public String toValue() {
+        return cookies.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + KEY_VALUE_DELIMITER + entry.getValue())
+                .collect(Collectors.joining(COOKIE_DELIMITER));
+    }
+}
--- tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseHeaders.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/HttpResponseHeaders.java
@@ -1,27 +1,35 @@
 package org.apache.coyote.http11.response;
 
+import org.apache.coyote.http11.HttpCookie;
 import org.apache.coyote.http11.MimeType;
 
 import java.nio.charset.Charset;
 
-public record HttpResponseHeaders(MimeType mimeType, Charset charset, Location location) {
+public record HttpResponseHeaders(MimeType mimeType, Charset charset, Location location, HttpCookie cookie) {
 
     public HttpResponseHeaders(MimeType mimeType) {
-        this(mimeType, Charset.defaultCharset(), null);
+        this(mimeType, Charset.defaultCharset(), null, null);
     }
 
     public HttpResponseHeaders(Location location) {
-        this(MimeType.ALL, Charset.defaultCharset(), location);
+        this(MimeType.ALL, Charset.defaultCharset(), location, null);
     }
 
     public HttpResponseHeaders(MimeType mimeType, Location location) {
-        this(mimeType, Charset.defaultCharset(), location);
+        this(mimeType, Charset.defaultCharset(), location, null);
+    }
+
+    public HttpResponseHeaders(MimeType mimeType, Location location, HttpCookie httpCookie) {
+        this(mimeType, Charset.defaultCharset(), location, httpCookie);
     }
 
     public String toMessage() {
         String message = "Content-Type: " + mimeType.getDescription() + ";charset=" + charset.name().toLowerCase() + " \r\n";
         if (location != null) {
             message += "Location: " + location.url() + " \r\n";
+        }
+        if (cookie != null) {
+            message += "Set-Cookie: " + cookie.toValue() + " \r\n";
         }
         return message;
     }
