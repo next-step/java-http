8b7e82e748bcbf6e54f6e64b082294764e0f6d6a
youngbinary <youngbin@barogo.com>
2024-07-19 01:45:52 +0900

    feat: 404 이미지 렌더링 하기위한 ContentType 추가

--- tomcat/src/main/java/org/apache/coyote/http11/model/ContentType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/model/ContentType.java
@@ -11,6 +11,7 @@ public enum ContentType {
     TEXT_HTML( "text/html", "html"),
     TEXT_CSS("text/css", "css"),
     TEXT_JS("text/javascript", "js"),
+    IMAGE_SVG( "image/svg+xml", "svg"),
     ;
 
     private final String type;
