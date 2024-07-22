680b1c1070eb956de4e14b363aed56be24cdb85a
jeongho <qkr2435@everon.co.kr>
2024-07-23 00:52:18 +0900

    test: 요구사항 테스트 코드 정리

---
+++ tomcat/src/test/java/camp/nextstep/http/PathResolverTest.java
@@ -0,0 +1,18 @@
+package camp.nextstep.http;
+
+import org.junit.jupiter.api.Test;
+
+class PathResolverTest {
+
+  @Test
+  void getFilePath() {
+  }
+
+  @Test
+  void getHttpHeader() {
+  }
+
+  @Test
+  void of() {
+  }
+}
--- tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
+++ tomcat/src/test/java/nextstep/org/apache/coyote/http11/Http11ProcessorTest.java
@@ -56,7 +56,7 @@ class Http11ProcessorTest {
     var expected = String.join("\r\n",
         "HTTP/1.1 200 OK ",
         "Content-Type: text/html;charset=utf-8 ",
-        "Content-Length: 5564 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
+        "Content-Length: 556434 ", // 운영체제 환경에 따라 다른 값이 나올 수 있음. 자신의 개발 환경에 맞춰 수정할 것.
         "",
         new String(Files.readAllBytes(new File(resource.getFile()).toPath())));
 
@@ -106,4 +106,117 @@ class Http11ProcessorTest {
 
     assertThat(socket.output()).isEqualTo(expected);
   }
+
+  @Test
+  @DisplayName("로그인 성공시 HTTP Status Code 302를 반환하고, /index.html로 리다이렉트 한다.")
+  void loginSuccess() throws IOException {
+    // given
+    final String httpRequest = String.join("\r\n",
+        "GET /login?account=gugu&password=password HTTP/1.1 ",
+        "Host: localhost:8080 ",
+        "Connection: keep-alive ",
+        "",
+        "");
+
+    final var socket = new StubSocket(httpRequest);
+    final Http11Processor processor = new Http11Processor(socket);
+
+    // when
+    processor.process(socket);
+
+    // then
+    var expected = String.join("\r\n",
+        "HTTP/1.1 302 Found ",
+        "Location: /index.html ",
+        "",
+        "");
+    assertThat(socket.output()).isEqualTo(expected);
+  }
+
+
+  @Test
+  @DisplayName(" 로그인 성공하면 session을 만들고, 브라우저가 set-cookie를 통해 cookie에 session 정보를  반환한다 . ")
+  void cookie() {
+    // given
+    final String httpRequest = String.join("\r\n",
+        "GET /login?account=gugu&password=password HTTP/1.1 ",
+        "Host: localhost:8080 ",
+        "Connection: keep-alive ",
+        "",
+        "");
+
+    final var socket = new StubSocket(httpRequest);
+    final Http11Processor processor = new Http11Processor(socket);
+
+    // when
+    processor.process(socket);
+
+    // then
+    var expected = String.join("\r\n",
+        "HTTP/1.1 302 Found ",
+        "Set-Cookie: JSESSIONID=656cef62-e3c4-40bc-a8df-94732920ed46  ",
+        "Location: http://localhost:8080/index.html ",
+        "",
+        "");
+
+    assertThat(socket.output()).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName(" 로그인이 되어 있을 경우, set-cookie 에 session 정보를 반환하며 index.html 페이지로 이동한다. ")
+  void login() {
+    // given
+    final String httpRequest = String.join("\r\n",
+        "GET / HTTP/1.1 ",
+        "Host: localhost:8080 ",
+        "Connection: keep-alive ",
+        "Cookie: JSESSIONID=656cef62-e3c4-40bc-a8df-94732920ed46  ",
+
+        "",
+        "");
+
+    final var socket = new StubSocket(httpRequest);
+    final Http11Processor processor = new Http11Processor(socket);
+
+    // when
+    processor.process(socket);
+
+    // then
+    final URL resource = getClass().getClassLoader().getResource("static/css/styles.css");
+    var expected = String.join("\r\n",
+        "HTTP/1.1 302 Found ",
+        "Location: http://localhost:8080/index.html ",
+        "",
+        "");
+
+    assertThat(socket.output()).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName(" 로그인 하지 않았을 경우, login.html 페이지로 이동한다. ")
+  void isLogin() {
+    // given
+    final String httpRequest = String.join("\r\n",
+        "GET / HTTP/1.1 ",
+        "Host: localhost:8080 ",
+        "Connection: keep-alive ",
+        "",
+        "");
+
+    final var socket = new StubSocket(httpRequest);
+    final Http11Processor processor = new Http11Processor(socket);
+
+    // when
+    processor.process(socket);
+
+    // then
+    var expected = String.join("\r\n",
+        "HTTP/1.1 302 Found ",
+        "Location: http://localhost:8080/login.html ",
+        "",
+        "");
+
+    assertThat(socket.output()).isEqualTo(expected);
+  }
+
 }
