d2b088c07fcf8af67f7eb0e661b742a93741b18b
jeongho <qkr2435@everon.co.kr>
2024-07-23 02:47:51 +0900

    feat: process 책임 분리

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,20 +2,17 @@ package org.apache.coyote.http11;
 
 import camp.nextstep.exception.InvalidRequestException;
 import camp.nextstep.exception.UncheckedServletException;
-import camp.nextstep.http.ContentType;
 import camp.nextstep.http.HttpHeaders;
 import camp.nextstep.http.HttpRequest;
-import camp.nextstep.http.PathResolver;
+import camp.nextstep.http.HttpResponse;
+import camp.nextstep.http.RequestHandler;
 import camp.nextstep.http.RequestLine;
-import camp.nextstep.util.FileUtils;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.StringJoiner;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,13 +20,13 @@ import org.slf4j.LoggerFactory;
 public class Http11Processor implements Runnable, Processor {
 
   private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
-  public static final String staticPath = "static/";
-  public static final String PATH_SEPARATOR = "/";
-
+  private final RequestHandler requestHandler;
   private final Socket connection;
 
   public Http11Processor(final Socket connection) {
     this.connection = connection;
+    this.requestHandler = new RequestHandler();
+
   }
 
   @Override
@@ -47,26 +44,11 @@ public class Http11Processor implements Runnable, Processor {
       if (bufferedInputStream.lines() == null) {
         throw new InvalidRequestException("request가 null이거나 비어있습니다.");
       }
-      var responseBody = "Hello world!";
 
-      //HttpRequest parsing from InputStream
       HttpRequest httpRequest = parseHttpRequest(bufferedInputStream);
+      HttpResponse httpResponse = requestHandler.handleRequest(httpRequest);
 
-      //handler 로 처리가능
-      PathResolver pathResolver = PathResolver.of(httpRequest.getRequestLine().getPath());
-      String extension = FileUtils.getFileExtension(pathResolver.getFilePath());
-
-      URL url = getClass().getClassLoader().getResource(staticPath + pathResolver.getFilePath());
-
-      if (url != null) {
-        responseBody = new String(Files.readAllBytes(new File(url.getFile()).toPath()));
-      }
-
-      final var response = String.join("\r\n", "HTTP/1.1 200 OK ",
-          "Content-Type: " + ContentType.getTypeByExtention(extension) + ";charset=utf-8 ",
-          "Content-Length: " + responseBody.getBytes().length + " ", "", responseBody);
-
-      outputStream.write(response.getBytes());
+      outputStream.write(httpResponse.buildResponse().getBytes());
       outputStream.flush();
     } catch (IOException | UncheckedServletException e) {
       log.error(e.getMessage(), e);
@@ -75,25 +57,33 @@ public class Http11Processor implements Runnable, Processor {
 
   private HttpRequest parseHttpRequest(final BufferedReader requestStream) throws IOException {
     RequestLine requestLine = RequestLine.parse(requestStream.readLine());
-    String paseMessage = readRequestByLine(requestStream);
-    HttpHeaders requestHeader = new HttpHeaders(paseMessage);
 
-    return new HttpRequest(requestLine, requestHeader);
+    List<String> headerLines = new ArrayList<>();
+    String line;
+    while ((line = requestStream.readLine()) != null && !line.isEmpty()) {
+      headerLines.add(line);
+    }
 
+    HttpHeaders headers = parseRequestHeader(requestStream);
+
+    StringBuilder requestBody = new StringBuilder();
+    while (requestStream.ready()) {
+      requestBody.append((char) requestStream.read());
+    }
+
+    return new HttpRequest(requestLine, headers, requestBody.toString());
   }
 
-  private String readRequestByLine(final BufferedReader requestStream) throws IOException {
-
-    StringJoiner stringJoiner = new StringJoiner("\r\n");
-
-    while (requestStream.ready()) {
-      final String line = requestStream.readLine();
-      if (line == null || line.isEmpty()) {
+  private HttpHeaders parseRequestHeader(final BufferedReader inputReader) throws IOException {
+    final var requestHeaders = new ArrayList<String>();
+    while (inputReader.ready()) {
+      final var line = inputReader.readLine();
+      if (line.isEmpty()) {
         break;
       }
-      stringJoiner.add(line);
+      requestHeaders.add(line);
     }
-    return stringJoiner.toString();
+    return new HttpHeaders(requestHeaders);
   }
 
 
