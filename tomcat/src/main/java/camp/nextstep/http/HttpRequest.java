a5eee76857d6450adb6145cbefb2a41abb30fb49
jeongho <qkr2435@everon.co.kr>
2024-07-23 02:47:38 +0900

    feat: HttpResponse 객체 생성

--- tomcat/src/main/java/camp/nextstep/http/HttpRequest.java
+++ tomcat/src/main/java/camp/nextstep/http/HttpRequest.java
@@ -3,19 +3,24 @@ package camp.nextstep.http;
 public class HttpRequest {
 
   private final RequestLine requestLine;
-  private final HttpHeaders httpHeaders;
+  private final HttpHeaders headers;
+  private final String requestBody;
+
+  public HttpRequest(RequestLine requestLine, HttpHeaders headers, String requestBody) {
+    this.requestLine = requestLine;
+    this.headers = headers;
+    this.requestBody = requestBody;
+  }
 
   public RequestLine getRequestLine() {
     return requestLine;
   }
 
-  public HttpHeaders getHttpHeaders() {
-    return httpHeaders;
+  public HttpHeaders getHeaders() {
+    return headers;
   }
 
-  public HttpRequest(RequestLine requestLine, HttpHeaders httpHeaders) {
-    this.requestLine = requestLine;
-    this.httpHeaders = httpHeaders;
+  public String getRequestBody() {
+    return requestBody;
   }
-
 }
---
+++ tomcat/src/main/java/camp/nextstep/http/HttpResponse.java
@@ -0,0 +1,45 @@
+package camp.nextstep.http;
+
+public class HttpResponse {
+
+  private final HttpStatus status;
+  private final HttpHeaders headers;
+  private final byte[] body;
+
+  private HttpResponse(HttpStatus status, HttpHeaders headers, byte[] body) {
+    this.status = status;
+    this.headers = headers;
+    this.body = body;
+  }
+
+  public static HttpResponse ok(String body, String contentType) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", contentType + ";charset=utf-8");
+    return new HttpResponse(HttpStatus.OK, headers, body.getBytes());
+  }
+
+  public static HttpResponse redirect(String location) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Location", location);
+    return new HttpResponse(HttpStatus.FOUND, headers, new byte[0]);
+  }
+
+  public static HttpResponse error(HttpStatus status, String message) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "text/plain;charset=utf-8");
+    return new HttpResponse(status, headers, message.getBytes());
+  }
+
+  public String buildResponse() {
+    StringBuilder response = new StringBuilder();
+    response.append(
+        String.format("HTTP/1.1 %s %s\r\n", status.getStatusCode(), status.getReasonPhrase()));
+    response.append(headers.toString());
+    response.append("\r\n");
+    if (body.length > 0) {
+      response.append(new String(body));
+    }
+    return response.toString();
+  }
+
+}
