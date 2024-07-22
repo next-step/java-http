0c61a13a69936981d0c92f8869bdc11d92a4bbdf
Heedoitdox <73086575+heedoitdox@users.noreply.github.com>
2024-07-22 09:29:33 +0900

    🚀 5단계 - 동시성 확장하기 (#68)
    * refactor: 자식클래스에서 method body 정의할 메서드 abstract 메서드로 변경
    
    * docs: 5단계 동시성 확장하기 요구명세 문서 작성
    
    * feat: SessionManager SESSION 값 동시성 컬렉션 적용 (3단계에서 선적용)
    
    * feat: Connector 에서 ThreadPoolExecutor 를 적용하여 동시성 확장
    
    ---------
    
    Co-authored-by: youngbinary <youngbin@barogo.com>

--- tomcat/src/main/java/org/apache/catalina/connector/Connector.java
+++ tomcat/src/main/java/org/apache/catalina/connector/Connector.java
@@ -1,5 +1,9 @@
 package org.apache.catalina.connector;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import org.apache.coyote.http11.Http11Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +19,13 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8080;
     private static final int DEFAULT_ACCEPT_COUNT = 100;
+    public static final int DEFAULT_THREAD_CORE_POOL_SIZE = 200; // 기본적으로 요청을 처리할 thread 값
+    public static final int DEFAULT_THREAD_MAXIMUM_POOL_SIZE = 250; // 요청을 처리할 thread 최대 값
+    public static final long DEFAULT_THREAD_KEEP_ALIVE_TIME = 0L; // core thread 수 보다 많은 thread 가 생성되는 경우 추가된 thread 를 정리하기 위한 대기 시간
+    public static final int DEFAULT_THREAD_QUEUE_CAPACITY = 100; // core thread 수 이상의 요청일 들어올 때 대기하게 되는 queue 의 capacity
 
     private final ServerSocket serverSocket;
+    private final ExecutorService executorService;
     private boolean stopped;
 
     public Connector() {
@@ -25,7 +34,26 @@ public class Connector implements Runnable {
 
     public Connector(final int port, final int acceptCount) {
         this.serverSocket = createServerSocket(port, acceptCount);
+        this.executorService = new ThreadPoolExecutor(
+            DEFAULT_THREAD_CORE_POOL_SIZE,
+            DEFAULT_THREAD_MAXIMUM_POOL_SIZE,
+            DEFAULT_THREAD_KEEP_ALIVE_TIME,
+            TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<>(DEFAULT_THREAD_QUEUE_CAPACITY)
+        );
         this.stopped = false;
+    }
+
+    /* 기본 thread 풀 설정을 사용하고 싶지 않을 때 커스텀 할 수 있는 생성자 */
+    public Connector(final int port, final int acceptCount, final int maxThreads) {
+        this.serverSocket = createServerSocket(port, acceptCount);
+        this.executorService = new ThreadPoolExecutor(
+            DEFAULT_THREAD_CORE_POOL_SIZE,
+            maxThreads, // 최대로 요청을 처리할 수 있는 thread 수
+            DEFAULT_THREAD_KEEP_ALIVE_TIME,
+            TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<>(acceptCount) // thread 를 대기시킬 queue 사용량
+        );
     }
 
     private ServerSocket createServerSocket(final int port, final int acceptCount) {
@@ -67,7 +95,7 @@ public class Connector implements Runnable {
             return;
         }
         var processor = new Http11Processor(connection);
-        new Thread(processor).start();
+        executorService.execute(processor);
     }
 
     public void stop() {
