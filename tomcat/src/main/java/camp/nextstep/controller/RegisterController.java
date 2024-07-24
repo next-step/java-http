a21482371dc040fab1d5eb31bf4509e4ddfad7e4
qkr2435 <qkr2435@gmail.com>
2024-07-25 01:38:17 +0900

    feat: 상속받은 Controller 생성

---
+++ tomcat/src/main/java/camp/nextstep/controller/RegisterController.java
@@ -0,0 +1,12 @@
+package camp.nextstep.controller;
+
+import camp.nextstep.http.HttpRequest;
+import camp.nextstep.http.HttpResponse;
+
+public class RegisterController extends AbstractController{
+
+    @Override
+    public HttpResponse service(HttpRequest httpRequest) throws Exception {
+        return super.service(httpRequest);
+    }
+}
